9dffa7b0835b5e433a2a417d23fedfb97b267315
Photon <Photon-GitHub@users.noreply.github.com>
2017-07-30 13:21:07 +0200

    Logbot code cleanup.

--- src/main/java/de/photon/AACAdditionPro/addition/additions/LogBot.java
+++ src/main/java/de/photon/AACAdditionPro/addition/additions/LogBot.java
@@ -10,8 +10,15 @@ import java.util.concurrent.TimeUnit;
 
 public class LogBot implements Addition, Runnable
 {
-    private static final File AAC_LOG_FOLDER = new File("plugins/AAC", "logs");
-    private static final File AACADDITIONPRO_LOG_FOLDER = new File("plugins/AACAdditionPro", "logs");
+    private static final File[] AFFECTED_FOLDERS = new File[]
+            {
+                    // AAC-Folder for logs.
+                    new File("plugins/AAC", "logs"),
+
+                    // AACAdditionPro folder for logs.
+                    new File("plugins/AACAdditionPro", "logs")
+            };
+
     private final long millis_until_delete = TimeUnit.DAYS.toMillis(AACAdditionPro.getInstance().getConfig().getLong(this.getConfigString() + ".days_until_delete"));
 
     private int task_number;
@@ -19,38 +26,31 @@ public class LogBot implements Addition, Runnable
     @Override
     public void run()
     {
-        // AACAdditionPro
-        deleteOldFiles(AACADDITIONPRO_LOG_FOLDER, millis_until_delete);
+        for (File logFolder : AFFECTED_FOLDERS) {
+            final long current_time = System.currentTimeMillis();
 
-        // AAC
-        deleteOldFiles(AAC_LOG_FOLDER, millis_until_delete);
-    }
+            if (logFolder.exists()) {
+                final File[] files = logFolder.listFiles();
 
-    private static void deleteOldFiles(final File folder, final long millis_until_delete)
-    {
-        final long current_time = System.currentTimeMillis();
-
-        if (folder.exists()) {
-            final File[] files = folder.listFiles();
-
-            if (files != null) {
-                // The folder is not empty
-                for (final File file : files) {
-                    // Be sure it is a log file
-                    if (file.getName().endsWith(".log") &&
-                        // Minimum time
-                        current_time - file.lastModified() > millis_until_delete)
-                    {
-                        if (file.delete()) {
-                            VerboseSender.sendVerboseMessage("Deleted " + file.getName());
-                        } else {
-                            VerboseSender.sendVerboseMessage("Could not delete old file " + file.getName(), true, true);
+                if (files != null) {
+                    // The folder is not empty
+                    for (final File file : files) {
+                        // Be sure it is a log file
+                        if (file.getName().endsWith(".log") &&
+                            // Minimum time
+                            current_time - file.lastModified() > this.millis_until_delete)
+                        {
+                            if (file.delete()) {
+                                VerboseSender.sendVerboseMessage("Deleted " + file.getName());
+                            } else {
+                                VerboseSender.sendVerboseMessage("Could not delete old file " + file.getName(), true, true);
+                            }
                         }
                     }
                 }
+            } else {
+                AACAdditionPro.getInstance().getLogger().severe("Could not find folder.");
             }
-        } else {
-            AACAdditionPro.getInstance().getLogger().severe("Could not find folder.");
         }
     }
 

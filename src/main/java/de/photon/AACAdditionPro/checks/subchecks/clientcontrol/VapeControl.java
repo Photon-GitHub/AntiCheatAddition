4c26cd3f7f5337e601f3c0df5e8e80cf52237c72
Photon <Photon-GitHub@users.noreply.github.com>
2017-08-13 21:45:26 +0200

    Rest of Vapecontrol

--- src/main/java/de/photon/AACAdditionPro/checks/subchecks/clientcontrol/VapeControl.java
+++ src/main/java/de/photon/AACAdditionPro/checks/subchecks/clientcontrol/VapeControl.java
@@ -6,12 +6,16 @@ import de.photon.AACAdditionPro.checks.ClientControlCheck;
 import de.photon.AACAdditionPro.userdata.User;
 import de.photon.AACAdditionPro.userdata.UserManager;
 import de.photon.AACAdditionPro.util.files.LoadFromConfiguration;
+import de.photon.AACAdditionPro.util.verbose.VerboseSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
 import java.util.List;
 
-public class VapeControl implements PluginMessageListener, ClientControlCheck
+public class VapeControl implements Listener, PluginMessageListener, ClientControlCheck
 {
     @LoadFromConfiguration(configPath = ".commands_on_detection", listType = String.class)
     private List<String> commandsOnDetection;
@@ -28,6 +32,12 @@ public class VapeControl implements PluginMessageListener, ClientControlCheck
         return AdditionHackType.WORLDDOWNLOAD_CONTROL;
     }
 
+    @EventHandler
+    public void on(PlayerJoinEvent event)
+    {
+        event.getPlayer().sendMessage("§8 §8 §1 §3 §3 §7 §8 ");
+    }
+
     @Override
     public void onPluginMessageReceived(final String channel, final Player player, final byte[] message)
     {
@@ -37,6 +47,15 @@ public class VapeControl implements PluginMessageListener, ClientControlCheck
             return;
         }
 
+        String clientData;
+
+        try {
+            clientData = new String(message);
+        } catch (Exception e) {
+            clientData = "";
+        }
+
+        VerboseSender.sendVerboseMessage("Player " + player.getName() + " joined with Vape | Data: " + clientData);
         executeCommands(player);
     }
 

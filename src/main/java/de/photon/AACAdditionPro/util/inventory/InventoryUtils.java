50ef5c28d3638d28df9a8c9dd8d4a3b921f599e8
Photon-GitHub <tgar001@fam-roehn.de>
2017-12-12 15:40:11 +0100

    locateSlot

--- src/main/java/de/photon/AACAdditionPro/util/inventory/InventoryUtils.java
+++ src/main/java/de/photon/AACAdditionPro/util/inventory/InventoryUtils.java
@@ -3,10 +3,256 @@ package de.photon.AACAdditionPro.util.inventory;
 import de.photon.AACAdditionPro.AACAdditionPro;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
 
 public final class InventoryUtils
 {
+    /**
+     * Used to locate a slot in an {@link org.bukkit.inventory.Inventory}.
+     * The coordinate-system is (0|0) in upper-left corner.
+     * <br>
+     * Please make sure that the {@link Player} is not riding a horse, as this method does not support horses/donkeys/mules
+     *
+     * @param rawSlot the number that is returned by getRawSlot()
+     * @return the coords of a slot or null if it is invalid.
+     */
+    public static float[] locateSlot(int rawSlot, InventoryType inventoryType)
+    {
+        System.out.println(rawSlot + " | " + inventoryType);
+        switch (inventoryType)
+        {
+            case CHEST:
+            case ENDER_CHEST:
+                // TODO: MAKE SURE THAT THE PLAYER IS NOT RIDING A HORSE, THAT IS ALSO COUNTED AS CHEST.
+                // Chest and Enderchest have the same layout
+                /*
+                 * 0                         -                       8
+                 *
+                 * 9                         -                       17
+                 *
+                 * 18                        -                       26
+                 * ------------------------------------------------------
+                 * ------------------------------------------------------
+                 * 27                        -                       35
+                 *
+                 * 36                        -                       44
+                 *
+                 * 45                        -                       53
+                 * ------------------------------------------------------
+                 * 54                        -                       62
+                 */
+                return new float[]{
+                        rawSlot % 9,
+                        rawSlot / 9
+                };
+            case DISPENSER:
+            case DROPPER:
+                // Dispenser and Dropper have the same layout
+                /*
+                 *                   0       1       2
+                 *
+                 *                   3       4       5
+                 *
+                 *                   6       7       8
+                 * ------------------------------------------------------
+                 * ------------------------------------------------------
+                 * 9                         -                       17
+                 *
+                 * 18                        -                       26
+                 *
+                 * 27                        -                       35
+                 * ------------------------------------------------------
+                 * 36                        -                       44
+                 */
+                // In the dispenser - part
+                if (rawSlot < 9)
+                {
+                    return new float[]{
+                            4 + rawSlot % 3,
+                            rawSlot / 3
+                    };
+                }
 
+                return new float[]{
+                        rawSlot % 9,
+                        // It is 3.5 away, and / 9 will always result in something >= 1
+                        2.5F + rawSlot / 9
+                };
+            case FURNACE:
+                /*
+                 *                 0
+                 *
+                 *                                    2
+                 *
+                 *                 1
+                 * ------------------------------------------------------
+                 * ------------------------------------------------------
+                 * 3                         -                       11
+                 *
+                 * 12                        -                       20
+                 *
+                 * 21                        -                       29
+                 * ------------------------------------------------------
+                 * 30                        -                       38
+                 */
+                switch (rawSlot)
+                {
+                    case 0:
+                        return new float[]{
+                                3.5F,
+                                0F
+                        };
+                    case 1:
+                        return new float[]{
+                                3.5F,
+                                2F
+                        };
+                    case 2:
+                        return new float[]{
+                                7F,
+                                1F
+                        };
+                    default:
+                        return new float[]{
+                                (rawSlot - 3) % 9,
+                                // It is 3.5 away, and (x - 3) / 9 will result in something >= 0
+                                3.5F + (rawSlot - 3) / 9
+                        };
+                }
+            case WORKBENCH:
+                /*
+                 *          1       2       3
+                 *
+                 *          4       5       6            0
+                 *
+                 *          7       8       9
+                 * ------------------------------------------------------
+                 * ------------------------------------------------------
+                 * 10                         -                      18
+                 *
+                 * 19                        -                       27
+                 *
+                 * 28                        -                       36
+                 * ------------------------------------------------------
+                 * 37                        -                       45
+                 */
+                break;
+            case CRAFTING:
+                /* Player Inventory
+                 * 5
+                 *
+                 * 6                            1   2
+                 *                                         ->      0
+                 * 7                            3   4
+                 *
+                 * 8
+                 * ------------------------------------------------------
+                 * 9                         -                       17
+                 *
+                 * 18                        -                       26
+                 *
+                 * 27                        -                       35
+                 * ------------------------------------------------------
+                 * 36                        -                       44
+                 * */
+                break;
+            case ENCHANTING:
+                /*
+                 *
+                 *
+                 *    0    1
+                 *
+                 *
+                 * ------------------------------------------------------
+                 * ------------------------------------------------------
+                 * 2                         -                       10
+                 *
+                 * 11                        -                       19
+                 *
+                 * 20                        -                       28
+                 * ------------------------------------------------------
+                 * 29                        -                       37
+                 */
+                break;
+            case MERCHANT:
+                /*
+                 *
+                 *
+                 *       0           1                      2
+                 *
+                 *
+                 * ------------------------------------------------------
+                 * ------------------------------------------------------
+                 * 3                         -                       11
+                 *
+                 * 12                        -                       20
+                 *
+                 * 21                        -                       29
+                 * ------------------------------------------------------
+                 * 30                        -                       38
+                 */
+                break;
+            case ANVIL:
+                /*
+                 *
+                 *
+                 *       0                 1                      2
+                 *
+                 *
+                 * ------------------------------------------------------
+                 * ------------------------------------------------------
+                 * 3                         -                       11
+                 *
+                 * 12                        -                       20
+                 *
+                 * 21                        -                       29
+                 * ------------------------------------------------------
+                 * 30                        -                       38
+                 */
+                break;
+            case BEACON:
+                /*
+                 *
+                 *
+                 *                                        0
+                 *
+                 *
+                 * ------------------------------------------------------
+                 * ------------------------------------------------------
+                 * 1                         -                       9
+                 *
+                 * 10                        -                       18
+                 *
+                 * 19                        -                       27
+                 * ------------------------------------------------------
+                 * 28                        -                       36
+                 */
+                break;
+            case HOPPER:
+                /*
+                 *
+                 *
+                 *           0       1       2       3       4
+                 *
+                 *
+                 * ------------------------------------------------------
+                 * ------------------------------------------------------
+                 * 5                         -                       13
+                 *
+                 * 14                        -                       22
+                 *
+                 * 23                        -                       31
+                 * ------------------------------------------------------
+                 * 32                        -                       40
+                 */
+                break;
+            default:
+                // CREATIVE (false positives), PLAYER, SHULKER_BOX, BREWING_STAND (version compatibility)
+                return null;
+        }
+        return null;
+    }
+    
     /**
      * This schedules an {@link org.bukkit.inventory.Inventory} update to be executed synchronously in the next server tick
      *

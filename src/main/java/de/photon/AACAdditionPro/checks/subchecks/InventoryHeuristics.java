b948240771ba73af8fe5302f0b108c1e0d57d35a
Photon-GitHub <tgar001@fam-roehn.de>
2018-01-13 15:14:44 +0100

    Removed debug.

--- src/main/java/de/photon/AACAdditionPro/checks/subchecks/InventoryHeuristics.java
+++ src/main/java/de/photon/AACAdditionPro/checks/subchecks/InventoryHeuristics.java
@@ -144,7 +144,6 @@ public class InventoryHeuristics implements Listener, ViolationModule
             for (Map.Entry<Pattern, Double> entry : outputDataMap.entrySet())
             {
                 // Pattern testing
-                VerboseSender.sendVerboseMessage("DEBUG: Player " + user.getPlayer().getName() + " has been detected by pattern " + entry.getKey().getName());
                 double value = entry.getValue();
                 //TODO: THIS IS ONLY A WORKAROUND FOR THE 0.5 PROBLEM!!!
                 if (value >= 0.5)

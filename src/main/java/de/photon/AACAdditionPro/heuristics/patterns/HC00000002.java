195691320ac29d394de39f298c1bf2e99b11bd74
Photon-GitHub <tgar001@fam-roehn.de>
2018-01-20 20:31:02 +0100

    Changed the HC2 pattern.

--- src/main/java/de/photon/AACAdditionPro/heuristics/patterns/HC00000002.java
+++ src/main/java/de/photon/AACAdditionPro/heuristics/patterns/HC00000002.java
@@ -12,6 +12,17 @@ import java.util.Map;
  */
 public class HC00000002 extends Pattern
 {
+    /**
+     * A threshold after which an inventory interaction is flagged as idling and thus not evaluated.
+     */
+    private static final double IDLE_TIME_THRESHOLD = 2000;
+
+    /**
+     * How many idle interactions are allowed before the dataset is flagged as not eligible for evaluation.
+     */
+    private static final double MAX_IDLE_TIME_COUNTER = 4;
+
+
     public HC00000002()
     {
         super("HC000000002", new InputData[]{
@@ -27,9 +38,17 @@ public class HC00000002 extends Pattern
 
         // Use a offset sum to detect too consistent clicking.
         // orElse(0) is ok as the steam (and thus the array) must be empty to reach this part of code.
-        final double average = Arrays.stream(inputArray[0]).average().orElse(0);
-        double offsetSum = MathUtils.offsetSum(inputArray[0], average, offset -> true);
+        final double[] idleRemovedData = Arrays.stream(inputArray[0]).filter(offset -> offset < IDLE_TIME_THRESHOLD).toArray();
 
-        return Math.pow(Math.E, -0.05 * (offsetSum * offsetSum));
+        if (idleRemovedData.length < inputArray.length - MAX_IDLE_TIME_COUNTER)
+        {
+            // Too few datasets.
+            return 0D;
+        }
+
+        final double average = Arrays.stream(idleRemovedData).average().orElse(0);
+        final double offsetSum = MathUtils.offsetSum(idleRemovedData, average, offset -> true);
+
+        return Math.pow(Math.E, -10 * (offsetSum * offsetSum));
     }
 }

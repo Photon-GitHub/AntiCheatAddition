07cd72f2e7608de3ea393121e95c0ea473872fa7
Photon-GitHub <tgar001@fam-roehn.de>
2018-01-31 20:08:34 +0100

    Reformat.

--- src/main/java/de/photon/AACAdditionPro/util/world/EntityUtils.java
+++ src/main/java/de/photon/AACAdditionPro/util/world/EntityUtils.java
@@ -25,7 +25,8 @@ public final class EntityUtils
     {
         final List<Player> nearbyPlayers = new ArrayList<>(5);
 
-        for (final Player player : initialPlayer.getWorld().getPlayers()) {
+        for (final Player player : initialPlayer.getWorld().getPlayers())
+        {
             if (!initialPlayer.getUniqueId().equals(player.getUniqueId()) &&
                 // Check coordinates
                 MathUtils.areLocationsInRange(initialPlayer.getLocation(), player.getLocation(), squaredDistance))
@@ -47,45 +48,22 @@ public final class EntityUtils
      *
      * @return a {@link List} of {@link Player}s who are in range.
      */
-    public static List<Player> getNearbyPlayers(final Player initialPlayer, final double x, final double y, final double z)
-    {
-        final List<Player> nearbyPlayers = new ArrayList<>(5);
-
-        for (final Player player : initialPlayer.getWorld().getPlayers()) {
-            if (!initialPlayer.getUniqueId().equals(player.getUniqueId()) &&
-                // Check coordinates
-                MathUtils.areLocationsInRange(initialPlayer.getLocation(), player.getLocation(), x, y, z))
-            {
-                nearbyPlayers.add(player);
-            }
-        }
-
-        return nearbyPlayers;
-    }
-
-    /**
-     * Gets all {@link Player}s who are in a certain range of the given {@link Player}
-     *
-     * @param initialPlayer the location from which the distance is measured
-     * @param x             the maximum x-distance between the initialPlayer and the checked {@link Player} to add the checked {@link Player} to the {@link List}.
-     * @param y             the maximum y-distance between the initialPlayer and the checked {@link Player} to add the checked {@link Player} to the {@link List}.
-     * @param z             the maximum z-distance between the initialPlayer and the checked {@link Player} to add the checked {@link Player} to the {@link List}.
-     *
-     * @return a {@link List} of {@link Player}s who are in range.
-     */
     public static List<LivingEntity> getLivingEntitiesAroundPlayer(final Player initialPlayer, final double x, final double y, final double z)
     {
         final List<LivingEntity> initialLivingEntities;
-        try {
+        try
+        {
             initialLivingEntities = Bukkit.getScheduler().callSyncMethod(AACAdditionPro.getInstance(), () -> initialPlayer.getWorld().getLivingEntities()).get();
-        } catch (InterruptedException | ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e)
+        {
             e.printStackTrace();
             return Collections.emptyList();
         }
 
         final List<LivingEntity> nearbyLivingEntities = new ArrayList<>(5);
 
-        for (final LivingEntity livingEntity : initialLivingEntities) {
+        for (final LivingEntity livingEntity : initialLivingEntities)
+        {
             if (!initialPlayer.getUniqueId().equals(livingEntity.getUniqueId()) &&
                 // Check coordinates
                 MathUtils.areLocationsInRange(initialPlayer.getLocation(), livingEntity.getLocation(), x, y, z))

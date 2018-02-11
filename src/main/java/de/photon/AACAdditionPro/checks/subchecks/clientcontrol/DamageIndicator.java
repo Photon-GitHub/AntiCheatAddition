11e80563e9cd1ac755efed97808fe48bda3ca7b1
Photon-GitHub <tgar001@fam-roehn.de>
2018-02-12 00:57:03 +0100

    Bugfix.

--- src/main/java/de/photon/AACAdditionPro/checks/subchecks/clientcontrol/DamageIndicator.java
+++ src/main/java/de/photon/AACAdditionPro/checks/subchecks/clientcontrol/DamageIndicator.java
@@ -70,7 +70,7 @@ public class DamageIndicator extends PacketAdapter implements Module
                 {
                     case MC188:
                         // Add spoofed health
-                        wrappedWatchableObjects.add(new WrappedWatchableObject(7, 20));
+                        wrappedWatchableObjects.add(new WrappedWatchableObject(6, 20F));
                         break;
 
                     case MC110:

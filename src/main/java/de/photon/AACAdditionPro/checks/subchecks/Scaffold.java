49a65a085b797a6de0f12bd0104df6ee34f1081d
Photon-GitHub <tgar001@fam-roehn.de>
2018-02-13 21:27:47 +0100

    Reformat.

--- src/main/java/de/photon/AACAdditionPro/checks/subchecks/Scaffold.java
+++ src/main/java/de/photon/AACAdditionPro/checks/subchecks/Scaffold.java
@@ -143,7 +143,7 @@ public class Scaffold implements Listener, ViolationModule
                     user.getScaffoldData().sprintingFails--;
                 }
             }
-            
+
 
             // ----------------------------------------- Suspicious stops ------------------------------------------- //
 

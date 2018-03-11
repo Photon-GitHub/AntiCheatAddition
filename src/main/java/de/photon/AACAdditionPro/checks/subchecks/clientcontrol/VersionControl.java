32e67a302d67f6b57874044705b2ed8cc6fa9749
Photon-GitHub <tgar001@fam-roehn.de>
2018-03-11 13:56:01 +0100

    Added config layout for Version control.

---
+++ src/main/java/de/photon/AACAdditionPro/checks/subchecks/clientcontrol/VersionControl.java
@@ -0,0 +1,12 @@
+package de.photon.AACAdditionPro.checks.subchecks.clientcontrol;
+
+import us.myles.ViaVersion.api.Via;
+import us.myles.ViaVersion.api.ViaAPI;
+
+public class VersionControl
+{
+    public void doSometing()
+    {
+        ViaAPI api = Via.getAPI(); // Get the API
+    }
+}

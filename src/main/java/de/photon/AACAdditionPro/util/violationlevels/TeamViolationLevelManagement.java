0a94c8c47f716b2f5512ce24140198c2a0983d0a
Photon-GitHub <tgar001@fam-roehn.de>
2018-03-28 22:06:24 +0200

    ViolationLevelManagement cleanup.

--- src/main/java/de/photon/AACAdditionPro/util/violationlevels/TeamViolationLevelManagement.java
+++ src/main/java/de/photon/AACAdditionPro/util/violationlevels/TeamViolationLevelManagement.java
@@ -26,7 +26,8 @@ public class TeamViolationLevelManagement extends ViolationLevelManagement
     public int getTeamVl(final List<UUID> uuids)
     {
         int vlSum = 0;
-        for (final UUID uuid : uuids) {
+        for (final UUID uuid : uuids)
+        {
             vlSum += this.getVL(uuid);
         }
         return vlSum;
@@ -58,38 +59,32 @@ public class TeamViolationLevelManagement extends ViolationLevelManagement
 
     public void flagTeam(final List<Player> players, final int vl_increase, final int cancel_vl, final Runnable onCancel, final Runnable specialCode)
     {
-        this.flagTeam(players, vl_increase, cancel_vl, onCancel, specialCode, true);
-    }
-
-    /**
-     * Flags a whole team.
-     *
-     * @param players     the players that should be flagged.
-     * @param vl_increase how much the vl should be increased.
-     * @param cancel_vl   the ViolationLevel up from which onCancel is run. Set to -1 to disable
-     * @param onCancel    a {@link Runnable} that is executed if the vl is higher that cancel_vl
-     * @param specialCode a {@link Runnable} to define special code such as critical_vl
-     * @param punish      should the affected team be punished
-     */
-
-    public void flagTeam(final List<Player> players, final int vl_increase, final int cancel_vl, final Runnable onCancel, final Runnable specialCode, boolean punish)
-    {
         final List<UUID> uuids = new ArrayList<>(players.size());
 
-        for (final Player player : players) {
+        for (final Player player : players)
+        {
             uuids.add(player.getUniqueId());
             this.flag(player, vl_increase, cancel_vl, onCancel, specialCode);
         }
 
-        if (punish) {
-            punishTeam(players, this.getTeamVl(uuids));
-        }
+
+        punishTeam(players, this.getTeamVl(uuids));
     }
 
     @Override
     public void setVL(final Player player, final int newVl)
     {
-        this.setVL(player, newVl, false);
+        // A value smaller than 0 can be removed
+        if (newVl > 0 &&
+            // If the player is offline he can be removed
+            player.isOnline())
+        {
+            violationLevels.put(player.getUniqueId(), newVl);
+        }
+        else
+        {
+            violationLevels.remove(player.getUniqueId());
+        }
     }
 
     /**
@@ -103,13 +98,13 @@ public class TeamViolationLevelManagement extends ViolationLevelManagement
         thresholds.keySet().stream()
                   // Filter out all elements that are too big
                   .filter((integer) -> integer <= teamVL)
-                  // Reversed comparator to make sure the biggest element is the first one.
-                  .sorted((i1, i2) -> Integer.compare(i2, i1)).findFirst()
+                  // Find the biggest element below teamVL
+                  .max(Integer::compare)
                   // Make sure the element exists
                   .ifPresent(vl -> {
                       // Execute the commands
-                      for (final String s : thresholds.get(vl)) {
-
+                      for (final String s : thresholds.get(vl))
+                      {
                           // Command cannot be null as of the new loading process.
                           final String realCommand = Placeholders.applyPlaceholders(s, playersOfTeam, null);
 

16bab3213f84ee0fca3728eac918a9e4e42fe3a3
Photon-GitHub <tgar001@fam-roehn.de>
2018-03-29 23:49:09 +0200

    Set new initialLine.

--- src/main/java/de/photon/AACAdditionPro/util/files/configs/ConfigurationRepresentation.java
+++ src/main/java/de/photon/AACAdditionPro/util/files/configs/ConfigurationRepresentation.java
@@ -118,6 +118,7 @@ public class ConfigurationRepresentation
                     }
                 }
             }
+            configLines.set(initialLineIndex, initialLine);
         });
     }
 

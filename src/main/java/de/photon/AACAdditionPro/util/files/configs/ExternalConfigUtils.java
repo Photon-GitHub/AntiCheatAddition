99e5cb946eb24ce64facac1f3c58ffc0aede8b42
Photon-GitHub <tgar001@fam-roehn.de>
2018-03-29 15:58:47 +0200

    Comment handling.

--- src/main/java/de/photon/AACAdditionPro/util/files/configs/ExternalConfigUtils.java
+++ src/main/java/de/photon/AACAdditionPro/util/files/configs/ExternalConfigUtils.java
@@ -1,12 +1,11 @@
 package de.photon.AACAdditionPro.util.files.configs;
 
+import de.photon.AACAdditionPro.exceptions.ConfigException;
 import de.photon.AACAdditionPro.util.VerboseSender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.EnumMap;
 
 public final class ExternalConfigUtils
@@ -21,9 +20,9 @@ public final class ExternalConfigUtils
     public static void changeConfigs()
     {
         // Set the values
-        configChangeRequests.forEach((config, requestedChange) -> config.configYAML.set(requestedChange.key, requestedChange.value));
+        configChangeRequests.forEach((config, requestedChange) -> config.configuration.set(requestedChange.key, requestedChange.value));
         // Save the config.
-        configChangeRequests.keySet().forEach(ExternalConfigs::saveYAML);
+        configChangeRequests.keySet().forEach(ExternalConfigs::save);
     }
 
     public enum ExternalConfigs
@@ -34,25 +33,31 @@ public final class ExternalConfigUtils
 
         private final File configFile;
         @Getter
-        private final YamlConfiguration configYAML;
+        private FileConfig configuration;
 
         ExternalConfigs(final String path)
         {
             this.configFile = new File(path);
-            this.configYAML = YamlConfiguration.loadConfiguration(this.configFile);
+            try
+            {
+                this.configuration = new FileConfig(this.configFile);
+            } catch (ConfigException e)
+            {
+                VerboseSender.sendVerboseMessage("SEVERE: Failed to load config " + configFile.getName());
+                e.printStackTrace();
+            }
         }
 
-        private void saveYAML()
+        private void save()
         {
             try
             {
-                this.configYAML.save(configFile);
-            } catch (IOException e)
+                this.configuration.save();
+            } catch (ConfigException e)
             {
                 VerboseSender.sendVerboseMessage("Failed to modify " + this.name() + " config.", true, true);
                 e.printStackTrace();
             }
-
         }
     }
 

688e6d91537d2948614b06851e8d6e7c5664d3ff
Photon-GitHub <tgar001@fam-roehn.de>
2018-05-27 01:18:53 +0200

    InventoryData: Prevent some bypasses, move to a singleton listener instance.

--- src/main/java/de/photon/AACAdditionPro/user/data/InventoryData.java
+++ src/main/java/de/photon/AACAdditionPro/user/data/InventoryData.java
@@ -3,23 +3,31 @@ package de.photon.AACAdditionPro.user.data;
 import de.photon.AACAdditionPro.AACAdditionPro;
 import de.photon.AACAdditionPro.user.TimeData;
 import de.photon.AACAdditionPro.user.User;
+import de.photon.AACAdditionPro.user.UserManager;
+import de.photon.AACAdditionPro.util.world.BlockUtils;
 import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
-public class InventoryData extends TimeData implements Listener
+public class InventoryData extends TimeData
 {
+    static
+    {
+        AACAdditionPro.getInstance().registerListener(new InventoryDataUpdater());
+    }
+
     /**
      * The last slot a person clicked.<br>
      * This variable is used to prevent false positives based on spam-clicking one slot.
@@ -35,7 +43,6 @@ public class InventoryData extends TimeData implements Listener
         // [0] = Time of opening the inventory (or first click)
         // [1] = Latest click
         super(user, 0, 0);
-        AACAdditionPro.getInstance().registerListener(this);
     }
 
     public boolean notRecentlyOpened(final long milliseconds)
@@ -56,65 +63,115 @@ public class InventoryData extends TimeData implements Listener
         return this.getTimeStamp(0) != 0;
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void on(final PlayerDeathEvent event)
+    /**
+     * A singleton class to reduce the reqired {@link Listener}s to a minimum.
+     */
+    private static class InventoryDataUpdater implements Listener
     {
-        this.nullifyIfRefersToUser(event.getEntity().getUniqueId(), 0);
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void on(final PlayerRespawnEvent event)
-    {
-        this.nullifyIfRefersToUser(event.getPlayer().getUniqueId(), 0);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void on(final InventoryOpenEvent event)
-    {
-        // Removed theUser.getPlayer().getOpenInventory().getType() != InventoryType.CRAFTING.
-        this.updateIfRefersToUser(event.getPlayer().getUniqueId(), 0);
-    }
-
-    // Low to be after the MultiInteract EventHandler.
-    @EventHandler(priority = EventPriority.LOW)
-    public void on(final InventoryClickEvent event)
-    {
-        if (this.getUser().refersToUUID(event.getWhoClicked().getUniqueId()) &&
-            event.getSlotType() != InventoryType.SlotType.QUICKBAR)
+        @EventHandler(priority = EventPriority.MONITOR)
+        public void onDeath(final PlayerDeathEvent event)
         {
-            // Only update if the inventory is currently closed to not interfere with opening time checks.
-            if (!this.hasOpenInventory())
+            final User user = UserManager.getUser(event.getEntity().getUniqueId());
+
+            if (user != null)
             {
-                this.updateTimeStamp(0);
+                user.getInventoryData().nullifyTimeStamp(0);
             }
-            this.updateTimeStamp(1);
-            this.lastRawSlot = event.getRawSlot();
-            this.lastMaterial = event.getCurrentItem() == null ? Material.AIR : event.getCurrentItem().getType();
         }
-    }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void on(final InventoryCloseEvent event)
-    {
-        this.nullifyIfRefersToUser(event.getPlayer().getUniqueId(), 0);
-    }
+        @EventHandler(priority = EventPriority.MONITOR)
+        public void onRespawn(final PlayerRespawnEvent event)
+        {
+            final User user = UserManager.getUser(event.getPlayer().getUniqueId());
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void on(final PlayerTeleportEvent event)
-    {
-        this.nullifyIfRefersToUser(event.getPlayer().getUniqueId(), 0);
-    }
+            if (user != null)
+            {
+                user.getInventoryData().nullifyTimeStamp(0);
+            }
+        }
 
-    @EventHandler
-    public void on(final PlayerChangedWorldEvent event)
-    {
-        this.nullifyIfRefersToUser(event.getPlayer().getUniqueId(), 0);
-    }
+        @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+        public void onInteract(final PlayerInteractEvent event)
+        {
+            final User user = UserManager.getUser(event.getPlayer().getUniqueId());
 
-    @Override
-    public void unregister()
-    {
-        HandlerList.unregisterAll(this);
-        super.unregister();
+            if (user != null)
+            {
+                if (event.getAction() == Action.RIGHT_CLICK_BLOCK &&
+                    BlockUtils.CONTAINERS.contains(event.getClickedBlock().getType()))
+                {
+                    user.getInventoryData().updateTimeStamp(0);
+                }
+            }
+        }
+
+        @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+        public void onInventoryOpen(final InventoryOpenEvent event)
+        {
+            // Removed theUser.getPlayer().getOpenInventory().getType() != InventoryType.CRAFTING.
+            final User user = UserManager.getUser(event.getPlayer().getUniqueId());
+
+            if (user != null)
+            {
+                user.getInventoryData().updateTimeStamp(0);
+            }
+        }
+
+        // Low to be after the MultiInteract EventHandler.
+        @EventHandler(priority = EventPriority.LOW)
+        public void onInventoryClick(final InventoryClickEvent event)
+        {
+            final User user = UserManager.getUser(event.getWhoClicked().getUniqueId());
+
+            if (user != null)
+            {
+                if (event.getSlotType() != InventoryType.SlotType.QUICKBAR)
+                {
+                    // Only update if the inventory is currently closed to not interfere with opening time checks.
+                    if (!user.getInventoryData().hasOpenInventory())
+                    {
+                        user.getInventoryData().updateTimeStamp(0);
+                    }
+                    user.getInventoryData().updateTimeStamp(1);
+                    user.getInventoryData().lastRawSlot = event.getRawSlot();
+                    user.getInventoryData().lastMaterial = event.getCurrentItem() == null ?
+                                                           Material.AIR :
+                                                           event.getCurrentItem().getType();
+                }
+            }
+        }
+
+        @EventHandler(priority = EventPriority.MONITOR)
+        public void onInventoryClose(final InventoryCloseEvent event)
+        {
+            final User user = UserManager.getUser(event.getPlayer().getUniqueId());
+
+            if (user != null)
+            {
+                user.getInventoryData().nullifyTimeStamp(0);
+            }
+        }
+
+        @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+        public void onPlayerTeleport(final PlayerTeleportEvent event)
+        {
+            final User user = UserManager.getUser(event.getPlayer().getUniqueId());
+
+            if (user != null)
+            {
+                user.getInventoryData().nullifyTimeStamp(0);
+            }
+        }
+
+        @EventHandler
+        public void onWorldChange(final PlayerChangedWorldEvent event)
+        {
+            final User user = UserManager.getUser(event.getPlayer().getUniqueId());
+
+            if (user != null)
+            {
+                user.getInventoryData().nullifyTimeStamp(0);
+            }
+        }
     }
 }

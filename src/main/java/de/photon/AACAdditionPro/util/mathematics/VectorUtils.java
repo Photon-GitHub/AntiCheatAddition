e2cece5c11b47d77b10e7da397b641de93bf59cf
Photon-GitHub <tgar001@fam-roehn.de>
2018-07-22 02:33:01 +0200

    Bugfix and optimizations.

--- src/main/java/de/photon/AACAdditionPro/util/mathematics/VectorUtils.java
+++ src/main/java/de/photon/AACAdditionPro/util/mathematics/VectorUtils.java
@@ -13,22 +13,29 @@ public final class VectorUtils
      * Get to know where the {@link Vector} intersects with a {@link org.bukkit.block.Block}.
      * Non-Occluding {@link Block}s as defined in {@link BlockUtils#isReallyOccluding(Material)} are ignored.
      *
-     * @param start the starting {@link Location}
-     * @param a     the {@link Vector} which should be checked
+     * @param start     the starting {@link Location}
+     * @param direction the {@link Vector} which should be checked
      *
      * @return The length when the {@link Vector} intersects or 0 if no intersection was found
      */
-    public static double getDistanceToFirstIntersectionWithBlock(final Location start, final Vector a)
+    public static double getDistanceToFirstIntersectionWithBlock(final Location start, final Vector direction)
     {
-        final int length = (int) Math.floor(a.length());
+        final int length = (int) Math.floor(direction.length());
         if (length >= 1)
         {
             try
             {
-                final BlockIterator blockIterator = new BlockIterator(start.getWorld(), start.toVector(), a, 0, length);
+                // Chunk loading problem
+                if (!start.getChunk().isLoaded())
+                {
+                    return 0;
+                }
+
+                final BlockIterator blockIterator = new BlockIterator(start.getWorld(), start.toVector(), direction, 0, length);
+                Block block;
                 while (blockIterator.hasNext())
                 {
-                    final Block block = blockIterator.next();
+                    block = blockIterator.next();
                     // Account for a Spigot bug: BARRIER and MOB_SPAWNER are not occluding blocks
                     if (BlockUtils.isReallyOccluding(block.getType()))
                     {

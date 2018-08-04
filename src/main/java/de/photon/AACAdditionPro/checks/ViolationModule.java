d1fec1969e28f25becf5c6ea807b4eb3df80e913
Photon-GitHub <tgar001@fam-roehn.de>
2018-08-04 17:41:44 +0200

    Removed the default implementation of getViolationLevelManagement.

--- src/main/java/de/photon/AACAdditionPro/checks/ViolationModule.java
+++ src/main/java/de/photon/AACAdditionPro/checks/ViolationModule.java
@@ -10,8 +10,5 @@ public interface ViolationModule extends Module
      * @return the {@link ViolationLevelManagement} of the check.<br>
      * By default the check has no {@link ViolationLevelManagement} and this {@link java.lang.reflect.Method} throws a {@link NoViolationLevelManagementException}.
      */
-    default ViolationLevelManagement getViolationLevelManagement() throws NoViolationLevelManagementException
-    {
-        throw new NoViolationLevelManagementException(this.getModuleType());
-    }
+    ViolationLevelManagement getViolationLevelManagement();
 }

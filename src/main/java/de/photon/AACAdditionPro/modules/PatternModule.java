72f493af25711fb8de400c4365b3d6c80b6434b0
Photon-GitHub <tgar001@fam-roehn.de>
2018-08-14 17:12:05 +0200

    Add PatternModule.

---
+++ src/main/java/de/photon/AACAdditionPro/modules/PatternModule.java
@@ -0,0 +1,40 @@
+package de.photon.AACAdditionPro.modules;
+
+import java.util.Set;
+import java.util.function.Function;
+
+/**
+ * This indicates a {@link Module} which can be split up into different parts called {@link Pattern}s.
+ */
+public interface PatternModule extends Module
+{
+    /**
+     * @return a {@link Set} of all {@link Pattern}s managed by this {@link PatternModule}
+     */
+    Set<Pattern> getPatterns();
+    
+    static void enablePatterns(final PatternModule module)
+    {
+        for (Pattern pattern : module.getPatterns())
+        {
+            Module.enableModule(pattern);
+        }
+    }
+
+    /**
+     * Represents a single {@link Pattern} that is hold by a {@link PatternModule}
+     */
+    abstract class Pattern<T> implements Function<T, Integer>, Module
+    {
+        /**
+         * The name of the pattern in the config.
+         */
+        abstract String getName();
+
+        @Override
+        public String getConfigString()
+        {
+            return this.getModuleType().getConfigString() + '.' + this.getName();
+        }
+    }
+}

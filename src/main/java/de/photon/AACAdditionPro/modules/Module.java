05f925941bc6596364820ae0b356b50662e44fdf
Photon-GitHub <tgar001@fam-roehn.de>
2018-08-14 16:52:43 +0200

    Moved the LoadFromConfiguration routine to ConfigUtils.java.

--- src/main/java/de/photon/AACAdditionPro/modules/Module.java
+++ src/main/java/de/photon/AACAdditionPro/modules/Module.java
@@ -3,14 +3,6 @@ package de.photon.AACAdditionPro.modules;
 import de.photon.AACAdditionPro.AACAdditionPro;
 import de.photon.AACAdditionPro.util.VerboseSender;
 import de.photon.AACAdditionPro.util.files.configs.ConfigUtils;
-import de.photon.AACAdditionPro.util.files.configs.LoadFromConfiguration;
-import org.bukkit.Color;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Vector;
-
-import java.lang.reflect.Field;
-import java.util.List;
 
 public interface Module
 {
@@ -42,75 +34,8 @@ public interface Module
                 return;
             }
 
-            // Config-Annotation processing
-            LoadFromConfiguration annotation;
-            for (Field field : module.getClass().getDeclaredFields())
-            {
-                // Load the annotation and check if it is present.
-                annotation = field.getAnnotation(LoadFromConfiguration.class);
-
-                if (annotation == null)
-                    continue;
-
-                // Make it possible to modify the field
-                field.setAccessible(true);
-
-                // Get the full config path.
-                final String path = module.getConfigString() + annotation.configPath();
-
-                // Get the type of the field.
-                final Class clazz = field.getType();
-
-                // The different classes
-                try
-                {
-                    // Boolean
-                    if (clazz == boolean.class || clazz == Boolean.class)
-                        field.setBoolean(module, AACAdditionPro.getInstance().getConfig().getBoolean(path));
-                        // Numbers
-                    else if (clazz == double.class || clazz == Double.class)
-                        field.setDouble(module, AACAdditionPro.getInstance().getConfig().getDouble(path));
-                    else if (clazz == int.class || clazz == Integer.class)
-                        field.setInt(module, AACAdditionPro.getInstance().getConfig().getInt(path));
-                    else if (clazz == long.class || clazz == Long.class)
-                        field.setLong(module, AACAdditionPro.getInstance().getConfig().getLong(path));
-                    else if (clazz == String.class)
-                        field.set(module, AACAdditionPro.getInstance().getConfig().getString(path));
-                        // Special stuff
-                    else if (clazz == ItemStack.class)
-                        field.set(module, AACAdditionPro.getInstance().getConfig().getItemStack(path));
-                    else if (clazz == Color.class)
-                        field.set(module, AACAdditionPro.getInstance().getConfig().getColor(path));
-                    else if (clazz == OfflinePlayer.class)
-                        field.set(module, AACAdditionPro.getInstance().getConfig().getOfflinePlayer(path));
-                    else if (clazz == Vector.class)
-                        field.set(module, AACAdditionPro.getInstance().getConfig().getVector(path));
-                        // Lists
-                    else if (clazz == List.class)
-                    {
-                        // StringLists
-                        if (annotation.listType() == String.class)
-                        {
-                            field.set(module, ConfigUtils.loadStringOrStringList(path));
-
-                            // Unknown type
-                        }
-                        else
-                        {
-                            field.set(module, AACAdditionPro.getInstance().getConfig().getList(path));
-                        }
-
-                    }
-                    // No special type found
-                    else
-                    {
-                        field.set(module, AACAdditionPro.getInstance().getConfig().get(path));
-                    }
-                } catch (IllegalAccessException e)
-                {
-                    e.printStackTrace();
-                }
-            }
+            // Load the config values
+            ConfigUtils.processLoadFromConfiguration(module, module.getConfigString());
 
             if (module instanceof ListenerModule)
                 ListenerModule.enable((ListenerModule) module);
--- src/main/java/de/photon/AACAdditionPro/util/files/configs/ConfigUtils.java
+++ src/main/java/de/photon/AACAdditionPro/util/files/configs/ConfigUtils.java
@@ -1,8 +1,14 @@
 package de.photon.AACAdditionPro.util.files.configs;
 
 import de.photon.AACAdditionPro.AACAdditionPro;
+import de.photon.AACAdditionPro.modules.Module;
+import org.bukkit.Color;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
+import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +19,90 @@ import java.util.concurrent.ConcurrentMap;
 
 public final class ConfigUtils
 {
+    /**
+     * This will process the {@link LoadFromConfiguration} annotation to load all config values.
+     *
+     * @param object  the object in which the config values should be loaded.
+     * @param prePath the first path of the part (e.g. helpful when using {@link Module#getConfigString()})
+     */
+    public static void processLoadFromConfiguration(final Object object, String prePath)
+    {
+        // Config-Annotation processing
+        LoadFromConfiguration annotation;
+        for (Field field : object.getClass().getDeclaredFields())
+        {
+            // Load the annotation and check if it is present.
+            annotation = field.getAnnotation(LoadFromConfiguration.class);
+
+            if (annotation == null)
+                continue;
+
+            // Make it possible to modify the field
+            field.setAccessible(true);
+
+            // Get the full config path.
+            String path = annotation.configPath();
+
+            if (prePath != null)
+            {
+                path = prePath + path;
+            }
+
+            // Get the type of the field.
+            final Class type = field.getType();
+
+            // The different classes
+            try
+            {
+                // Boolean
+                if (type == boolean.class || type == Boolean.class)
+                    field.setBoolean(object, AACAdditionPro.getInstance().getConfig().getBoolean(path));
+                    // Numbers
+                else if (type == double.class || type == Double.class)
+                    field.setDouble(object, AACAdditionPro.getInstance().getConfig().getDouble(path));
+                else if (type == int.class || type == Integer.class)
+                    field.setInt(object, AACAdditionPro.getInstance().getConfig().getInt(path));
+                else if (type == long.class || type == Long.class)
+                    field.setLong(object, AACAdditionPro.getInstance().getConfig().getLong(path));
+                else if (type == String.class)
+                    field.set(object, AACAdditionPro.getInstance().getConfig().getString(path));
+                    // Special stuff
+                else if (type == ItemStack.class)
+                    field.set(object, AACAdditionPro.getInstance().getConfig().getItemStack(path));
+                else if (type == Color.class)
+                    field.set(object, AACAdditionPro.getInstance().getConfig().getColor(path));
+                else if (type == OfflinePlayer.class)
+                    field.set(object, AACAdditionPro.getInstance().getConfig().getOfflinePlayer(path));
+                else if (type == Vector.class)
+                    field.set(object, AACAdditionPro.getInstance().getConfig().getVector(path));
+                    // Lists
+                else if (type == List.class)
+                {
+                    // StringLists
+                    if (annotation.listType() == String.class)
+                    {
+                        field.set(object, ConfigUtils.loadStringOrStringList(path));
+
+                        // Unknown type
+                    }
+                    else
+                    {
+                        field.set(object, AACAdditionPro.getInstance().getConfig().getList(path));
+                    }
+
+                }
+                // No special type found
+                else
+                {
+                    field.set(object, AACAdditionPro.getInstance().getConfig().get(path));
+                }
+            } catch (IllegalAccessException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
     /**
      * Used to load a {@link List} of {@link String}s if it is uncertain if the value of path
      * is a {@link String} or a {@link List} of {@link String}s

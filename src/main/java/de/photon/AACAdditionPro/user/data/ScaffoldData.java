e9047d785ab7ee9597df7b0583ec4ffd376e72db
Photon-GitHub <tgar001@fam-roehn.de>
2018-10-21 04:14:44 +0200

    Fix config mistakes.

--- src/main/java/de/photon/AACAdditionPro/user/data/ScaffoldData.java
+++ src/main/java/de/photon/AACAdditionPro/user/data/ScaffoldData.java
@@ -14,7 +14,7 @@ public class ScaffoldData extends TimeData
 {
     // Default buffer size is 6, being well tested.
     private static final int BUFFER_SIZE = 6;
-    // Use static here as Datas are often created.
+    // Use static here as ScaffoldDatas are often created.
     private static final double DELAY_NORMAL = AACAdditionPro.getInstance().getConfig().getInt(ModuleType.SCAFFOLD.getConfigString() + ".parts.average.delays.normal");
     private static final double SNEAKING_ADDITION = AACAdditionPro.getInstance().getConfig().getInt(ModuleType.SCAFFOLD.getConfigString() + ".parts.average.delays.sneaking_addition");
     private static final double SNEAKING_SLOW_ADDITION = AACAdditionPro.getInstance().getConfig().getInt(ModuleType.SCAFFOLD.getConfigString() + ".parts.average.delays.sneaking_slow_addition");

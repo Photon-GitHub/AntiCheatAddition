b131550d39b6111f1e835c6a5f89b0e275833172
Photon-GitHub <tgar001@fam-roehn.de>
2018-10-30 01:39:53 +0100

    Force bugfix versions of minecraft 1.11 and 1.12

--- src/main/java/de/photon/AACAdditionPro/ServerVersion.java
+++ src/main/java/de/photon/AACAdditionPro/ServerVersion.java
@@ -18,8 +18,8 @@ public enum ServerVersion
     MC188("1.8.8", true),
     MC19("1.9", false),
     MC110("1.10", false),
-    MC111("1.11", true),
-    MC112("1.12", true),
+    MC111("1.11.2", true),
+    MC112("1.12.2", true),
     MC113("1.13", true);
 
 

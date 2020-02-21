7fd786b194ab40d003e65f1a340e47aec6c92a30
Photon-GitHub <photonde@protonmail.com>
2020-02-21 22:23:45 +0100

    Fixes multiple bugs.

--- src/main/java/de/photon/aacadditionpro/modules/additions/GuiInject.java
+++ src/main/java/de/photon/aacadditionpro/modules/additions/GuiInject.java
@@ -28,6 +28,7 @@ public class GuiInject implements ListenerModule, RestrictedServerVersion
     private static final ItemStack AACADDITIONPRO_ITEMSTACK;
     private static final ItemStack BACK_ITEMSTACK;
     private static final String AACADDITIONPRO_TITLE = ChatColor.GOLD + "AACAdditionPro " + ChatColor.DARK_GRAY + "GUI | Checks";
+    public static final String AAC_GUI_TITLE = ChatColor.RED + "AAC " + ChatColor.DARK_GRAY + "GUI | Checks";
 
     static {
         AACADDITIONPRO_ITEMSTACK = new ItemStack(Material.DIAMOND);
@@ -59,7 +60,7 @@ public class GuiInject implements ListenerModule, RestrictedServerVersion
     @EventHandler
     public void onJoin(InventoryOpenEvent event)
     {
-        if (event.getView().getTitle().startsWith(ChatColor.RED + "AAC " + ChatColor.DARK_GRAY + "GUI | Checks")
+        if (event.getView().getTitle().startsWith(AAC_GUI_TITLE)
             && event.getView().getType() == InventoryType.CHEST
             && InternalPermission.hasPermission(event.getPlayer(), InternalPermission.AAC_MANAGE))
         {
@@ -73,12 +74,13 @@ public class GuiInject implements ListenerModule, RestrictedServerVersion
         if (event.getView().getType() == InventoryType.CHEST
             && InternalPermission.hasPermission(event.getWhoClicked(), InternalPermission.AAC_MANAGE))
         {
-            if (event.getView().getTitle().equals(ChatColor.RED + "AAC " + ChatColor.DARK_GRAY + "GUI | Checks")
-                && AACADDITIONPRO_ITEMSTACK.equals(event.getCurrentItem()))
+            if (event.getView().getTitle().equals(AAC_GUI_TITLE)
+                // Direct click or hotkey.
+                && (AACADDITIONPRO_ITEMSTACK.equals(event.getCurrentItem()) || event.getHotbarButton() == 3))
             {
                 event.setCancelled(true);
 
-                // closeInventory and openInventory should not be run from an ClickEvent handler.
+                // closeInventory and openInventory should not be run directly from an ClickEvent handler.
                 Bukkit.getScheduler().runTask(AACAdditionPro.getInstance(), () -> {
                     event.getWhoClicked().closeInventory();
 
@@ -147,14 +149,11 @@ public class GuiInject implements ListenerModule, RestrictedServerVersion
                             }
                         }
 
-                        if (toToggle != null) {
-                            if (clicked.getType() == Material.GREEN_TERRACOTTA) {
-                                clicked.setType(Material.GRAY_TERRACOTTA);
-                                AACAdditionPro.getInstance().getModuleManager().setStateOfModule(toToggle, false);
-                            } else {
-                                clicked.setType(Material.GREEN_TERRACOTTA);
-                                AACAdditionPro.getInstance().getModuleManager().setStateOfModule(toToggle, true);
-                            }
+                        if (toToggle != null && toToggle != ModuleType.GUI_INJECT) {
+                            AACAdditionPro.getInstance().getModuleManager().setStateOfModule(toToggle, !AACAdditionPro.getInstance().getModuleManager().getStateOfModule(toToggle));
+                            clicked.setType(AACAdditionPro.getInstance().getModuleManager().getStateOfModule(toToggle) ?
+                                            Material.GREEN_TERRACOTTA :
+                                            Material.GRAY_TERRACOTTA);
 
                             event.getInventory().setItem(event.getRawSlot(), clicked);
                             player.updateInventory();

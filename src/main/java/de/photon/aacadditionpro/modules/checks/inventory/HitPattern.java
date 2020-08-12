c8aea0fc6f14a3e4ee54892a3b58186808ec3afa
Photon-GitHub <photonde@protonmail.com>
2020-08-12 02:09:04 +0200

    HitPattern conversion

--- src/main/java/de/photon/aacadditionpro/modules/checks/inventory/HitPattern.java
+++ src/main/java/de/photon/aacadditionpro/modules/checks/inventory/HitPattern.java
@@ -1,39 +1,51 @@
 package de.photon.aacadditionpro.modules.checks.inventory;
 
+import de.photon.aacadditionpro.modules.ListenerModule;
 import de.photon.aacadditionpro.modules.ModuleType;
-import de.photon.aacadditionpro.modules.PatternModule;
 import de.photon.aacadditionpro.user.User;
+import de.photon.aacadditionpro.user.UserManager;
 import de.photon.aacadditionpro.util.files.configs.LoadFromConfiguration;
+import de.photon.aacadditionpro.util.messaging.VerboseSender;
 import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
-class HitPattern extends PatternModule.Pattern<User, EntityDamageByEntityEvent>
+class HitPattern implements ListenerModule
 {
     @LoadFromConfiguration(configPath = ".cancel_vl")
     @Getter
     private int cancelVl;
 
-    @Override
-    protected int process(User user, EntityDamageByEntityEvent event)
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event)
     {
-        // Is in Inventory (Detection)
-        if (user.hasOpenInventory() &&
-            // Have the inventory opened for some time
-            user.notRecentlyOpenedInventory(1000) &&
-            // Is a hit-attack
-            event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK)
-        {
-            message = "Inventory-Verbose | Player: " + user.getPlayer().getName() + " hit an entity while having an open inventory.";
-            return 10;
+        if (event.getDamager() instanceof Player) {
+            final User user = UserManager.getUser(event.getDamager().getUniqueId());
+
+            // Not bypassed
+            if (User.isUserInvalid(user, this.getModuleType())) {
+                return;
+            }
+
+            // Is in Inventory (Detection)
+            if (user.hasOpenInventory() &&
+                // Have the inventory opened for some time
+                user.notRecentlyOpenedInventory(1000) &&
+                // Is a hit-attack
+                event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK)
+            {
+                Inventory.getInstance().getViolationLevelManagement().flag(user.getPlayer(), 10, cancelVl, () -> event.setCancelled(true),
+                                                                           () -> VerboseSender.getInstance().sendVerboseMessage("Inventory-Verbose | Player: " + user.getPlayer().getName() + " hit an entity while having an open inventory."));
+            }
         }
-        return 0;
     }
 
     @Override
-    public void cancelAction(User user, EntityDamageByEntityEvent event)
+    public boolean isSubModule()
     {
-        event.setCancelled(true);
+        return true;
     }
 
     @Override

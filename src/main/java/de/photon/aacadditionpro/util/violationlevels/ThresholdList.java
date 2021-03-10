d84899fd6c3099d7c98923a8cdfa4ea038160b64
Photon-GitHub <photonde@protonmail.com>
2021-03-10 05:25:24 +0100

    Use SortedMap for less bug-prone code.

--- src/main/java/de/photon/aacadditionpro/util/violationlevels/ThresholdList.java
+++ src/main/java/de/photon/aacadditionpro/util/violationlevels/ThresholdList.java
@@ -1,43 +1,26 @@
 package de.photon.aacadditionpro.util.violationlevels;
 
-import com.google.common.collect.ImmutableList;
-import de.photon.aacadditionpro.ServerVersion;
+import com.google.common.collect.ImmutableSortedMap;
 import de.photon.aacadditionpro.util.files.configs.ConfigUtils;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.SortedMap;
 import java.util.stream.Collectors;
 
 public class ThresholdList
 {
-    /**
-     * A {@link List} of {@link Threshold}s which is guaranteed to be sorted.
-     */
     @Getter
-    private final List<Threshold> thresholds;
-    // Sorted set of the vls of the Thresholds for fast index evaluation.
-    private final int[] vls;
+    private final SortedMap<Integer, Threshold> thresholdMap;
 
     public ThresholdList(List<Threshold> thresholds)
     {
-        if (ServerVersion.supportsActiveServerVersion(ServerVersion.NON_188_VERSIONS)) {
-            List<Threshold> temp = new ArrayList<>(thresholds);
-            Collections.sort(temp);
-            this.thresholds = ImmutableList.copyOf(temp);
-        } else {
-            this.thresholds = ImmutableList.sortedCopyOf(thresholds);
-        }
-
-        vls = new int[thresholds.size()];
-        for (int i = 0; i < vls.length; ++i) {
-            vls[i] = thresholds.get(i).getVl();
-        }
+        final ImmutableSortedMap.Builder<Integer, Threshold> builder = ImmutableSortedMap.naturalOrder();
+        for (Threshold threshold : thresholds) builder.put(threshold.getVl(), threshold);
+        thresholdMap = builder.build();
     }
 
     /**
@@ -59,41 +42,12 @@ public class ThresholdList
     }
 
     /**
-     * Calculates the index of fromVl + 1 or the next higher index if not present.
-     * + 1 because we do not want to punish the current vl.
-     */
-    public int getFromIndex(int fromVl)
-    {
-        int index = Arrays.binarySearch(this.vls, fromVl + 1);
-        if (index < 0) index = -(index + 1);
-        return index;
-    }
-
-    /**
-     * Calculates the index of toVl or the next lower index if not present.
-     */
-    public int getToIndex(int toVl)
-    {
-        int index = Arrays.binarySearch(this.vls, toVl);
-        // +2 as that will allow us to drop one decrement operation afterwards to floor the result.
-        if (index < 0) index = -(index + 2);
-        return index;
-    }
-
-    /**
      * Used to execute the commands of the {@link Threshold}s in this  {@link ThresholdList}.
      */
     public void executeThresholds(int fromVl, int toVl, Collection<Player> players)
     {
-        final int fromIndex = this.getFromIndex(fromVl);
-        final int toIndex = this.getToIndex(toVl);
-
-        // There are no thresholds between fromVl and toVl.
-        if (fromIndex > toIndex) return;
-
-        // <= is intentional here.
-        for (int i = fromIndex; i <= toIndex; ++i) {
-            this.thresholds.get(i).executeCommandList(players);
-        }
+        // Exclusive "to" index -> toVl +1
+        final Collection<Threshold> toExecute = thresholdMap.subMap(fromVl, toVl + 1).values();
+        for (Threshold threshold : toExecute) threshold.executeCommandList(players);
     }
 }

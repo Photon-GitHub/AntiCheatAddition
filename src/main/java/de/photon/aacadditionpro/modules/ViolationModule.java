4070471b490b1fa11f3ed0fd485a765abdfd473c
Photon-GitHub <photonde@protonmail.com>
2021-04-27 18:43:21 +0200

    Add convenience method.

--- src/main/java/de/photon/aacadditionpro/modules/ViolationModule.java
+++ src/main/java/de/photon/aacadditionpro/modules/ViolationModule.java
@@ -1,7 +1,12 @@
 package de.photon.aacadditionpro.modules;
 
+import de.photon.aacadditionpro.util.violationlevels.ViolationAggregateManagement;
 import de.photon.aacadditionpro.util.violationlevels.ViolationManagement;
+import de.photon.aacadditionpro.util.violationlevels.threshold.ThresholdManagement;
 import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public abstract class ViolationModule extends Module
 {
@@ -10,6 +15,24 @@ public abstract class ViolationModule extends Module
     public ViolationModule(String configString)
     {
         super(configString);
+    }
+
+    public static ViolationModule parentOf(String configString, ViolationModule... children)
+    {
+        return new ViolationModule(configString)
+        {
+            @Override
+            protected ViolationManagement createViolationManagement()
+            {
+                return new ViolationAggregateManagement(this, ThresholdManagement.loadThresholds(this.getConfigString()), Arrays.stream(children).map(violationModule -> getManagement()).collect(Collectors.toSet()));
+            }
+
+            @Override
+            protected ModuleLoader createModuleLoader()
+            {
+                return ModuleLoader.builder(this).build();
+            }
+        };
     }
 
     protected abstract ViolationManagement createViolationManagement();

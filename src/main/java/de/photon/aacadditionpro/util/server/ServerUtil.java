10f418aac20de5717fe381c08384af642723d6d2
Photon-GitHub <photonde@protonmail.com>
2021-05-06 20:50:43 +0200

    Use RingBuffer in TPS calculation for less memory usage and faster traversal.

--- src/main/java/de/photon/aacadditionpro/util/server/ServerUtil.java
+++ src/main/java/de/photon/aacadditionpro/util/server/ServerUtil.java
@@ -1,17 +1,16 @@
 package de.photon.aacadditionpro.util.server;
 
+import de.photon.aacadditionpro.util.datastructure.buffer.RingBuffer;
 import de.photon.aacadditionproold.AACAdditionPro;
 import de.photon.aacadditionproold.util.reflection.ClassReflect;
 import de.photon.aacadditionproold.util.reflection.Reflect;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.val;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayDeque;
-import java.util.Collections;
-import java.util.Deque;
 import java.util.logging.Level;
 
 /**
@@ -21,24 +20,7 @@ import java.util.logging.Level;
 public final class ServerUtil
 {
     private static final ClassReflect CRAFTPLAYER_CLASS_REFLECT = Reflect.fromOBC("entity.CraftPlayer");
-
-    /**
-     * Reflects the real ping of a {@link Player} from the CraftPlayer class.
-     *
-     * @param player the {@link Player} whose ping should be returned.
-     *
-     * @return the ping of the player or 0 if an exception is thrown.
-     */
-    public static int getPing(final Player player)
-    {
-        final Object craftPlayer = CRAFTPLAYER_CLASS_REFLECT.method("getHandle").invoke(player);
-        try {
-            return craftPlayer.getClass().getDeclaredField("ping").getInt(craftPlayer);
-        } catch (IllegalAccessException | NoSuchFieldException e) {
-            AACAdditionPro.getInstance().getLogger().log(Level.WARNING, "Failed to retrieve ping of a player: ", e);
-            return 0;
-        }
-    }
+    private static final TPS TPS = new TPS(AACAdditionPro.getInstance());
 
     // Use ConditionalCommands logic.
     /*MIT License
@@ -62,7 +44,23 @@ public final class ServerUtil
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.*/
 
-    private static final TPS TPS = new TPS(AACAdditionPro.getInstance());
+    /**
+     * Reflects the real ping of a {@link Player} from the CraftPlayer class.
+     *
+     * @param player the {@link Player} whose ping should be returned.
+     *
+     * @return the ping of the player or 0 if an exception is thrown.
+     */
+    public static int getPing(final Player player)
+    {
+        val craftPlayer = CRAFTPLAYER_CLASS_REFLECT.method("getHandle").invoke(player);
+        try {
+            return craftPlayer.getClass().getDeclaredField("ping").getInt(craftPlayer);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            AACAdditionPro.getInstance().getLogger().log(Level.WARNING, "Failed to retrieve ping of a player: ", e);
+            return 0;
+        }
+    }
 
     /**
      * Gets the current TPS of the server.
@@ -74,44 +72,41 @@ public final class ServerUtil
 
     private static class TPS extends BukkitRunnable
     {
-        private final int resolution;
+        private static final int RESOLUTION = 40;
+        private final RingBuffer<Long> tickIntervals;
         private long lastTick;
-        private final Deque<Long> tickIntervals;
 
         private TPS(final Plugin plugin)
         {
-            this.resolution = 40;
             this.lastTick = System.currentTimeMillis();
-            this.tickIntervals = new ArrayDeque<>(Collections.nCopies(this.resolution, 50L));
+            this.tickIntervals = new RingBuffer<>(RESOLUTION);
+            // Init the RingBuffer with 50 millis for 20 TPS.
+            for (int i = 0; i < RESOLUTION; ++i) this.tickIntervals.add(50L);
             this.runTaskTimer(plugin, 1L, 1L);
         }
 
         double getCurrentTPS()
         {
-            try {
-                final double tps = 1000.0 / this.getDelta();
-                return (tps > 20.0) ? 20.0 : tps;
-            } catch (Exception e) {
-                return 20.0;
-            }
+            val delta = this.getDelta();
+            if (delta == 0) return 20.0;
+
+            // 1000 milliseconds per second, delta is also milliseconds -> ticks. As the maximum of ticks is 20, allow no value above 20.
+            return Math.min(1000.0 / delta, 20.0);
         }
 
         public void run()
         {
-            final long curr = System.currentTimeMillis();
-            final long delta = curr - this.lastTick;
+            val curr = System.currentTimeMillis();
+            val delta = curr - this.lastTick;
             this.lastTick = curr;
-            this.tickIntervals.removeFirst();
-            this.tickIntervals.addLast(delta);
+            this.tickIntervals.add(delta);
         }
 
         private double getDelta()
         {
-            int base = 0;
-            for (final long delta : this.tickIntervals) {
-                base += (int) delta;
-            }
-            return base / this.resolution;
+            long base = 0;
+            for (final long delta : this.tickIntervals) base += delta;
+            return (double) (base / RESOLUTION);
         }
     }
 }

c2d65ae76e60f581d0c7f2c7b009943457d1afb3
Photon-GitHub <photonde@protonmail.com>
2021-05-19 16:09:17 +0200

    Improve performance of BetterSprintingSentinel

--- src/main/java/de/photon/aacadditionpro/modules/sentinel/BetterSprintingSentinel.java
+++ src/main/java/de/photon/aacadditionpro/modules/sentinel/BetterSprintingSentinel.java
@@ -6,23 +6,62 @@ import de.photon.aacadditionpro.modules.ModuleLoader;
 import de.photon.aacadditionpro.user.User;
 import de.photon.aacadditionpro.util.config.LoadFromConfiguration;
 import de.photon.aacadditionpro.util.pluginmessage.MessageChannel;
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import lombok.val;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
+// API_DOCS of BetterSprinting:
+/*
+ * OUTCOMING PACKETS
+ * =================
+ *
+ *** [byte 0] [bool <enableSurvivalFlyBoost>] [bool <enableAllDirs>]
+ ***
+ *** Notifies the player about which non-vanilla settings are enabled on the server (both are disabled by default).
+ *** Sent to player when their [byte 0] message is processed, and either or both settings are enabled.
+ *** Sent to all players with the mod after using the '/bettersprinting setting (...)' command.
+ *
+ *
+ *** [byte 1]
+ ***
+ *** Disables basic functionality of the mod on client side.
+ *** Sent to player when their [byte 0] message is processed, and the server wants to disable the mod.
+ *** Sent to all players with the mod after using the '/bettersprinting disablemod true' command.
+ *
+ *
+ *** [byte 2]
+ ***
+ *** Re-enables basic functionality of the mod on client side.
+ *** Sent to all players with the mod after using the '/bettersprinting disablemod false' command.
+ */
 public class BetterSprintingSentinel extends SentinelModule
 {
-    private final List<Boolean> featureList = ImmutableList.of(!AACAdditionPro.getInstance().getConfig().getBoolean(this.getConfigString() + ".disable.survival_fly_boost"),
-                                                               !AACAdditionPro.getInstance().getConfig().getBoolean(this.getConfigString() + ".disable.enable_all_dirs"));
+    private final ByteBuf settingsBuf;
+    private final ByteBuf disableBuf;
+
     @LoadFromConfiguration(configPath = ".disable_mod")
     private boolean disable;
 
     public BetterSprintingSentinel()
     {
         super("BetterSprinting");
+
+        val featureList = ImmutableList.of(!AACAdditionPro.getInstance().getConfig().getBoolean(this.getConfigString() + ".disable.survival_fly_boost"),
+                                           !AACAdditionPro.getInstance().getConfig().getBoolean(this.getConfigString() + ".disable.enable_all_dirs"));
+
+        val settingsBuffer = Unpooled.buffer();
+        settingsBuffer.writeByte(0);
+
+        for (Boolean enable : featureList) settingsBuffer.writeBoolean(enable);
+        this.settingsBuf = settingsBuffer;
+
+        val disableBuffer = Unpooled.buffer();
+        // Bypassed players are already filtered out.
+        // The mod provides a method to disable it
+        disableBuffer.writeByte(this.disable ? 1 : 2);
+        this.disableBuf = disableBuffer;
     }
 
     @Override
@@ -33,30 +72,6 @@ public class BetterSprintingSentinel extends SentinelModule
 
         detection(user.getPlayer());
 
-        // API_DOCS of BetterSprinting:
-        /*
-         * OUTCOMING PACKETS
-         * =================
-         *
-         *** [byte 0] [bool <enableSurvivalFlyBoost>] [bool <enableAllDirs>]
-         ***
-         *** Notifies the player about which non-vanilla settings are enabled on the server (both are disabled by default).
-         *** Sent to player when their [byte 0] message is processed, and either or both settings are enabled.
-         *** Sent to all players with the mod after using the '/bettersprinting setting (...)' command.
-         *
-         *
-         *** [byte 1]
-         ***
-         *** Disables basic functionality of the mod on client side.
-         *** Sent to player when their [byte 0] message is processed, and the server wants to disable the mod.
-         *** Sent to all players with the mod after using the '/bettersprinting disablemod true' command.
-         *
-         *
-         *** [byte 2]
-         ***
-         *** Re-enables basic functionality of the mod on client side.
-         *** Sent to all players with the mod after using the '/bettersprinting disablemod false' command.
-         */
         final String sendChannel;
         switch (channel) {
             case "BSprint":
@@ -70,17 +85,8 @@ public class BetterSprintingSentinel extends SentinelModule
                 throw new IllegalStateException("Unknown channel");
         }
 
-        val settingsBuf = Unpooled.buffer();
-        settingsBuf.writeByte(0);
-
-        for (Boolean enable : featureList) settingsBuf.writeBoolean(enable);
-        user.getPlayer().sendPluginMessage(AACAdditionPro.getInstance(), sendChannel, settingsBuf.array());
-
-        val disableBuf = Unpooled.buffer();
-        // Bypassed players are already filtered out.
-        // The mod provides a method to disable it
-        disableBuf.writeByte(disable ? 1 : 2);
-        user.getPlayer().sendPluginMessage(AACAdditionPro.getInstance(), sendChannel, disableBuf.array());
+        user.getPlayer().sendPluginMessage(AACAdditionPro.getInstance(), sendChannel, this.settingsBuf.array());
+        user.getPlayer().sendPluginMessage(AACAdditionPro.getInstance(), sendChannel, this.disableBuf.array());
     }
 
     @Override

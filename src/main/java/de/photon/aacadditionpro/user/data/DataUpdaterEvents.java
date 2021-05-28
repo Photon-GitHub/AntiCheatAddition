0f1559799f6b4dc557b128daab1e0dfa2014cfaf
Photon-GitHub <photonde@protonmail.com>
2021-05-29 00:09:16 +0200

    Code cleanup.

--- src/main/java/de/photon/aacadditionpro/user/data/DataUpdaterEvents.java
+++ src/main/java/de/photon/aacadditionpro/user/data/DataUpdaterEvents.java
@@ -63,11 +63,10 @@ public final class DataUpdaterEvents implements Listener
     public void onConsume(final PlayerItemConsumeEvent event)
     {
         val user = User.getUser(event.getPlayer().getUniqueId());
+        if (user == null) return;
 
-        if (user != null) {
-            user.getTimestampMap().at(TimestampKey.LAST_CONSUME_EVENT).update();
-            user.getDataMap().setObject(DataKey.ObjectKey.LAST_CONSUMED_ITEM_STACK, event.getItem());
-        }
+        user.getTimestampMap().at(TimestampKey.LAST_CONSUME_EVENT).update();
+        user.getDataMap().setObject(DataKey.ObjectKey.LAST_CONSUMED_ITEM_STACK, event.getItem());
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -83,19 +82,17 @@ public final class DataUpdaterEvents implements Listener
         // Was hit
         if (event.getEntity() instanceof HumanEntity) {
             val user = User.getUser(event.getEntity().getUniqueId());
+            if (user == null) return;
 
-            if (user != null) {
-                user.getTimestampMap().at(TimestampKey.TEAMING_COMBAT_TAG).update();
-            }
+            user.getTimestampMap().at(TimestampKey.TEAMING_COMBAT_TAG).update();
         }
 
         // Hit somebody else
         if (event.getDamager() instanceof HumanEntity) {
             val user = User.getUser(event.getEntity().getUniqueId());
+            if (user == null) return;
 
-            if (user != null) {
-                user.getTimestampMap().at(TimestampKey.TEAMING_COMBAT_TAG).update();
-            }
+            user.getTimestampMap().at(TimestampKey.TEAMING_COMBAT_TAG).update();
         }
     }
 
@@ -132,25 +129,20 @@ public final class DataUpdaterEvents implements Listener
     public void onInventoryClick(final InventoryClickEvent event)
     {
         val user = User.getUser(event.getWhoClicked().getUniqueId());
+        // Quickbar actions can be performed outside the inventory.
+        if (user == null || event.getSlotType() == InventoryType.SlotType.QUICKBAR) return;
 
-        if (user != null &&
-            // Quickbar actions can be performed outside the inventory.
-            event.getSlotType() != InventoryType.SlotType.QUICKBAR)
-        {
-            // Only update if the inventory is currently closed to not interfere with opening time checks.
-            if (!user.hasOpenInventory()) {
-                user.getTimestampMap().at(TimestampKey.INVENTORY_OPENED).update();
-            }
+        // Only update if the inventory is currently closed to not interfere with opening time checks.
+        if (!user.hasOpenInventory()) user.getTimestampMap().at(TimestampKey.INVENTORY_OPENED).update();
 
-            user.getTimestampMap().at(TimestampKey.LAST_INVENTORY_CLICK).update();
-            if (event.getCurrentItem() != null) user.getTimestampMap().at(TimestampKey.LAST_INVENTORY_CLICK_ON_ITEM).update();
+        user.getTimestampMap().at(TimestampKey.LAST_INVENTORY_CLICK).update();
+        if (event.getCurrentItem() != null) user.getTimestampMap().at(TimestampKey.LAST_INVENTORY_CLICK_ON_ITEM).update();
 
 
-            user.getDataMap().setInt(DataKey.IntegerKey.LAST_RAW_SLOT_CLICKED, event.getRawSlot());
-            user.getDataMap().setObject(DataKey.ObjectKey.LAST_MATERIAL_CLICKED, event.getCurrentItem() == null ?
-                                                                                 Material.AIR :
-                                                                                 event.getCurrentItem().getType());
-        }
+        user.getDataMap().setInt(DataKey.IntegerKey.LAST_RAW_SLOT_CLICKED, event.getRawSlot());
+        user.getDataMap().setObject(DataKey.ObjectKey.LAST_MATERIAL_CLICKED, event.getCurrentItem() == null ?
+                                                                             Material.AIR :
+                                                                             event.getCurrentItem().getType());
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -183,29 +175,28 @@ public final class DataUpdaterEvents implements Listener
     public void onMove(final PlayerMoveEvent event)
     {
         val user = User.getUser(event.getPlayer().getUniqueId());
+        if (user == null || event.getTo() == null) return;
 
-        if (user != null) {
-            // Head + normal movement
-            user.getTimestampMap().at(TimestampKey.LAST_HEAD_OR_OTHER_MOVEMENT).update();
+        // Head + normal movement
+        user.getTimestampMap().at(TimestampKey.LAST_HEAD_OR_OTHER_MOVEMENT).update();
 
-            // xz movement only
-            if (event.getFrom().getX() != event.getTo().getX() ||
-                event.getFrom().getZ() != event.getTo().getZ())
-            {
-                user.getTimestampMap().at(TimestampKey.LAST_XYZ_MOVEMENT).update();
-                user.getTimestampMap().at(TimestampKey.LAST_XZ_MOVEMENT).update();
-            }
-            // Any non-head movement.
-            else if (event.getFrom().getY() != event.getTo().getY()) {
-                user.getTimestampMap().at(TimestampKey.LAST_XYZ_MOVEMENT).update();
-            }
+        // xz movement only
+        if (event.getFrom().getX() != event.getTo().getX() ||
+            event.getFrom().getZ() != event.getTo().getZ())
+        {
+            user.getTimestampMap().at(TimestampKey.LAST_XYZ_MOVEMENT).update();
+            user.getTimestampMap().at(TimestampKey.LAST_XZ_MOVEMENT).update();
+        }
+        // Any non-head movement.
+        else if (event.getFrom().getY() != event.getTo().getY()) {
+            user.getTimestampMap().at(TimestampKey.LAST_XYZ_MOVEMENT).update();
+        }
 
-            // Slime block -> Tower slime jump
-            if (event.getFrom().getY() < event.getTo().getY() && event.getFrom().getBlock().getRelative(BlockFace.DOWN).getType() == Material.SLIME_BLOCK) {
-                // Custom formula fitted from test data. Capped to make sure that cheat clients cannot give themselves infinite protection millis.
-                // 2000 is already unreasonable, even for very fast block placing.
-                user.getTimestampMap().at(TimestampKey.TOWER_SLIME_JUMP).setToFuture(Math.min((long) (550 * (event.getTo().getY() - event.getFrom().getY()) + 75), 2000L));
-            }
+        // Slime block -> Tower slime jump
+        if (event.getFrom().getY() < event.getTo().getY() && event.getFrom().getBlock().getRelative(BlockFace.DOWN).getType() == Material.SLIME_BLOCK) {
+            // Custom formula fitted from test data. Capped to make sure that cheat clients cannot give themselves infinite protection millis.
+            // 2000 is already unreasonable, even for very fast block placing.
+            user.getTimestampMap().at(TimestampKey.TOWER_SLIME_JUMP).setToFuture(Math.min((long) (550 * (event.getTo().getY() - event.getFrom().getY()) + 75), 2000L));
         }
     }
 
@@ -213,61 +204,56 @@ public final class DataUpdaterEvents implements Listener
     public void onRespawn(final PlayerRespawnEvent event)
     {
         val user = User.getUser(event.getPlayer().getUniqueId());
+        if (user == null) return;
 
-        if (user != null) {
-            user.getTimestampMap().at(TimestampKey.INVENTORY_OPENED).setToZero();
-            user.getTimestampMap().at(TimestampKey.LAST_TELEPORT).update();
-            user.getTimestampMap().at(TimestampKey.LAST_RESPAWN).update();
-        }
+        user.getTimestampMap().at(TimestampKey.INVENTORY_OPENED).setToZero();
+        user.getTimestampMap().at(TimestampKey.LAST_TELEPORT).update();
+        user.getTimestampMap().at(TimestampKey.LAST_RESPAWN).update();
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onTeleport(final PlayerTeleportEvent event)
     {
         val user = User.getUser(event.getPlayer().getUniqueId());
+        if (user == null) return;
 
-        if (user != null) {
-            user.getTimestampMap().at(TimestampKey.INVENTORY_OPENED).setToZero();
-            user.getTimestampMap().at(TimestampKey.LAST_TELEPORT).update();
-        }
+        user.getTimestampMap().at(TimestampKey.INVENTORY_OPENED).setToZero();
+        user.getTimestampMap().at(TimestampKey.LAST_TELEPORT).update();
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onToggleSneak(final PlayerToggleSneakEvent event)
     {
         val user = User.getUser(event.getPlayer().getUniqueId());
-        val sneak = event.isSneaking();
+        if (user == null) return;
 
-        if (user != null) {
-            user.getDataMap().setBoolean(DataKey.BooleanKey.SNEAKING, sneak);
-            if (!sneak) user.getDataMap().setLong(DataKey.LongKey.LAST_SNEAK_DURATION, user.getTimestampMap().at(TimestampKey.LAST_SNEAK_TOGGLE).passedTime());
-            user.getTimestampMap().at(TimestampKey.LAST_SNEAK_TOGGLE).update();
-        }
+        val sneak = event.isSneaking();
+        user.getDataMap().setBoolean(DataKey.BooleanKey.SNEAKING, sneak);
+        if (!sneak) user.getDataMap().setLong(DataKey.LongKey.LAST_SNEAK_DURATION, user.getTimestampMap().at(TimestampKey.LAST_SNEAK_TOGGLE).passedTime());
+        user.getTimestampMap().at(TimestampKey.LAST_SNEAK_TOGGLE).update();
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onToggleSprint(final PlayerToggleSprintEvent event)
     {
         val user = User.getUser(event.getPlayer().getUniqueId());
-        val sprint = event.isSprinting();
+        if (user == null) return;
 
-        if (user != null) {
-            user.getDataMap().setBoolean(DataKey.BooleanKey.SPRINTING, sprint);
-            if (!sprint) user.getDataMap().setLong(DataKey.LongKey.LAST_SPRINT_DURATION, user.getTimestampMap().at(TimestampKey.LAST_SPRINT_TOGGLE).passedTime());
-            user.getTimestampMap().at(TimestampKey.LAST_SPRINT_TOGGLE).update();
-        }
+        val sprint = event.isSprinting();
+        user.getDataMap().setBoolean(DataKey.BooleanKey.SPRINTING, sprint);
+        if (!sprint) user.getDataMap().setLong(DataKey.LongKey.LAST_SPRINT_DURATION, user.getTimestampMap().at(TimestampKey.LAST_SPRINT_TOGGLE).passedTime());
+        user.getTimestampMap().at(TimestampKey.LAST_SPRINT_TOGGLE).update();
     }
 
     @EventHandler
     public void onWorldChange(final PlayerChangedWorldEvent event)
     {
         val user = User.getUser(event.getPlayer().getUniqueId());
+        if (user == null) return;
 
-        if (user != null) {
-            user.getTimestampMap().at(TimestampKey.INVENTORY_OPENED).setToZero();
-            user.getTimestampMap().at(TimestampKey.LAST_TELEPORT).update();
-            user.getTimestampMap().at(TimestampKey.LAST_WORLD_CHANGE).update();
-        }
+        user.getTimestampMap().at(TimestampKey.INVENTORY_OPENED).setToZero();
+        user.getTimestampMap().at(TimestampKey.LAST_TELEPORT).update();
+        user.getTimestampMap().at(TimestampKey.LAST_WORLD_CHANGE).update();
     }
 
     /**

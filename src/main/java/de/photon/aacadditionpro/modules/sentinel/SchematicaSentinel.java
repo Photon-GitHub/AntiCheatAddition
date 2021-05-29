a240d4d5ac7db9f72a8a7ad9544b3897e741a5a3
Photon-GitHub <photonde@protonmail.com>
2021-05-29 16:31:22 +0200

    Deteting Schematica.

--- src/main/java/de/photon/aacadditionpro/modules/sentinel/SchematicaSentinel.java
+++ src/main/java/de/photon/aacadditionpro/modules/sentinel/SchematicaSentinel.java
@@ -9,12 +9,9 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import lombok.val;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class SchematicaSentinel extends SentinelModule implements Listener
+public class SchematicaSentinel extends SentinelModule
 {
     private static final MessageChannel SCHEMATICA_CHANNEL = MessageChannel.ofLegacy("schematica");
 
@@ -38,26 +35,24 @@ public class SchematicaSentinel extends SentinelModule implements Listener
         this.sentMessage = Unpooled.unmodifiableBuffer(byteBuf);
     }
 
-    @EventHandler
-    public void on(final PlayerJoinEvent event)
+    @Override
+    public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] message)
     {
-        val user = User.getUser(event.getPlayer().getUniqueId());
-        if (User.isUserInvalid(user, this)) return;
+        if (new String(message).contains("schematica")) {
+            val user = User.getUser(player.getUniqueId());
+            if (User.isUserInvalid(user, this)) return;
 
-        user.getPlayer().sendPluginMessage(AACAdditionPro.getInstance(), SCHEMATICA_CHANNEL.getChannel(), sentMessage.array());
+            detection(player);
+            user.getPlayer().sendPluginMessage(AACAdditionPro.getInstance(), SCHEMATICA_CHANNEL.getChannel(), sentMessage.array());
+        }
     }
 
     @Override
     protected ModuleLoader createModuleLoader()
     {
         return ModuleLoader.builder(this)
+                           .addIncomingMessageChannels(MessageChannel.of("minecraft", "register", "REGISTER"))
                            .addOutgoingMessageChannels(SCHEMATICA_CHANNEL)
                            .addAllowedServerVersions(ServerVersion.LEGACY_PLUGIN_MESSAGE_VERSIONS).build();
-    }
-
-    @Override
-    public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] message)
-    {
-        // Ignore.
     }
 }

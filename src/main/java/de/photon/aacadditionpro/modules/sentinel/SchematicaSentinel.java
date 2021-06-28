3abac61281dd111d4280d7002dfaacc22fa16168
Photon-GitHub <photonde@protonmail.com>
2021-06-28 12:08:01 +0200

    Don't listen to minecraft:register

--- src/main/java/de/photon/aacadditionpro/modules/sentinel/SchematicaSentinel.java
+++ src/main/java/de/photon/aacadditionpro/modules/sentinel/SchematicaSentinel.java
@@ -8,11 +8,15 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import lombok.val;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class SchematicaSentinel extends SentinelModule
+public class SchematicaSentinel extends SentinelModule implements Listener
 {
-    private static final MessageChannel SCHEMATICA_CHANNEL = MessageChannel.ofLegacy("schematica");
+    private static final String SCHEMATICA = "schematica";
+    private static final MessageChannel SCHEMATICA_CHANNEL = MessageChannel.ofLegacy(SCHEMATICA);
 
     private final ByteBuf sentMessage;
 
@@ -34,15 +38,22 @@ public class SchematicaSentinel extends SentinelModule
         this.sentMessage = Unpooled.unmodifiableBuffer(byteBuf);
     }
 
-    @Override
-    public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] message)
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerJoin(PlayerJoinEvent event)
     {
-        if (new String(message).contains("schematica")) {
-            val user = User.getUser(player);
-            if (User.isUserInvalid(user, this)) return;
+        val user = User.getUser(event.getPlayer());
+        if (User.isUserInvalid(user, this)) return;
 
-            detection(player);
-            user.getPlayer().sendPluginMessage(AACAdditionPro.getInstance(), SCHEMATICA_CHANNEL.getChannel(), sentMessage.array());
+        if (user.getPlayer().getListeningPluginChannels().contains(""))
+            detection(event.getPlayer());
+        user.getPlayer().sendPluginMessage(AACAdditionPro.getInstance(), SCHEMATICA_CHANNEL.getChannel(), sentMessage.array());
+
+        for (String channel : user.getPlayer().getListeningPluginChannels()) {
+            if (channel.equals(SCHEMATICA)) {
+                detection(user.getPlayer());
+                user.getPlayer().sendPluginMessage(AACAdditionPro.getInstance(), SCHEMATICA_CHANNEL.getChannel(), sentMessage.array());
+                return;
+            }
         }
     }
 
@@ -50,8 +61,13 @@ public class SchematicaSentinel extends SentinelModule
     protected ModuleLoader createModuleLoader()
     {
         return ModuleLoader.builder(this)
-                           .addIncomingMessageChannels(MessageChannel.of("minecraft", "register", "REGISTER"))
                            .addOutgoingMessageChannels(SCHEMATICA_CHANNEL)
                            .build();
+    }
+
+    @Override
+    public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] message)
+    {
+        // Ignore.
     }
 }

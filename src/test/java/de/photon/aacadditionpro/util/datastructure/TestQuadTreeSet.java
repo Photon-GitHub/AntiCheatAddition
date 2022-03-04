75bba9f0d9714cac11f37c32edaff83d0c72c731
Photon-GitHub <photonde@protonmail.com>
2022-03-04 02:19:57 +0100

    Add queue tests.

--- src/test/java/de/photon/aacadditionpro/util/datastructure/TestQuadTreeSet.java
+++ src/test/java/de/photon/aacadditionpro/util/datastructure/TestQuadTreeSet.java
@@ -1,5 +1,6 @@
 package de.photon.aacadditionpro.util.datastructure;
 
+import de.photon.aacadditionpro.util.datastructure.kdtree.QuadTreeQueue;
 import de.photon.aacadditionpro.util.datastructure.kdtree.QuadTreeSet;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import java.util.ArrayList;
 class TestQuadTreeSet
 {
     @Test
-    void emptyTest()
+    void emptySetTest()
     {
         var quad = new QuadTreeSet<Boolean>();
         Assertions.assertFalse(quad.iterator().hasNext());
@@ -17,7 +18,15 @@ class TestQuadTreeSet
     }
 
     @Test
-    void oneTest()
+    void emptyQueueTest()
+    {
+        var quad = new QuadTreeQueue<Boolean>();
+        Assertions.assertFalse(quad.iterator().hasNext());
+        Assertions.assertEquals(0, quad.size());
+    }
+
+    @Test
+    void oneSetTest()
     {
         var quad = new QuadTreeSet<Boolean>();
         quad.add(1, 1, false);
@@ -31,9 +40,41 @@ class TestQuadTreeSet
     }
 
     @Test
-    void removeIterationTest()
+    void oneQueueTest()
+    {
+        var quad = new QuadTreeQueue<Boolean>();
+        quad.add(1, 1, false);
+        Assertions.assertTrue(quad.iterator().hasNext());
+        Assertions.assertEquals(1, quad.size());
+
+        var list = new ArrayList<>(quad);
+        Assertions.assertEquals(1, list.size());
+        var expected = new QuadTreeSet.Node<>(1, 1, false);
+        Assertions.assertEquals(expected, list.get(0));
+    }
+
+    @Test
+    void removeSetIterationTest()
     {
         var quad = new QuadTreeSet<Boolean>();
+
+        for (int i = 0; i < 100; ++i) {
+            quad.add(i, i, false);
+        }
+
+        int x = quad.size();
+        while (!quad.isEmpty()) {
+            --x;
+            var any = quad.getAny();
+            quad.remove(any);
+            Assertions.assertEquals(x, quad.size());
+        }
+    }
+
+    @Test
+    void removeQueueIterationTest()
+    {
+        var quad = new QuadTreeQueue<Boolean>();
 
         for (int i = 0; i < 100; ++i) {
             quad.add(i, i, false);

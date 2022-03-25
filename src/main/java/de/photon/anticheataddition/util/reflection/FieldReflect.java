9807abeb1bceff0cf28b6f79e549d9545e4f669f
Photon-GitHub <photonde@protonmail.com>
2022-03-25 15:32:46 +0100

    Make TempValueReflect an inner class.

--- src/main/java/de/photon/anticheataddition/util/reflection/FieldReflect.java
+++ src/main/java/de/photon/anticheataddition/util/reflection/FieldReflect.java
@@ -1,9 +1,13 @@
 package de.photon.anticheataddition.util.reflection;
 
+import de.photon.anticheataddition.AntiCheatAddition;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
 
 @RequiredArgsConstructor
 public class FieldReflect
@@ -13,5 +17,67 @@ public class FieldReflect
     public TempValueReflect from(Object obj)
     {
         return new TempValueReflect(field, obj);
+    }
+
+    @RequiredArgsConstructor
+    public static class TempValueReflect
+    {
+        private final Field field;
+        private final Object obj;
+
+        public <T> T as(Class<T> clazz)
+        {
+            try {
+                return (T) this.field.get(obj);
+            } catch (IllegalAccessException e) {
+                AntiCheatAddition.getInstance().getLogger().log(Level.SEVERE, "Unable to get field as custom type via reflection", e);
+            }
+
+            return null;
+        }
+
+        public byte[] asBytes()
+        {
+            try {
+                return (byte[]) this.field.get(obj);
+            } catch (IllegalAccessException e) {
+                AntiCheatAddition.getInstance().getLogger().log(Level.SEVERE, "Unable to get field as byte array via reflection", e);
+            }
+
+            return new byte[0];
+        }
+
+        public double asDouble()
+        {
+            try {
+                return this.field.getDouble(obj);
+            } catch (IllegalAccessException e) {
+                AntiCheatAddition.getInstance().getLogger().log(Level.SEVERE, "Unable to get field as double via reflection", e);
+            }
+
+            return 0;
+        }
+
+        public <T> List<T> asList(Class<T> clazz)
+        {
+            try {
+                return (List<T>) this.field.get(obj);
+            } catch (IllegalAccessException e) {
+                AntiCheatAddition.getInstance().getLogger().log(Level.SEVERE, "Unable to get field as list via reflection", e);
+            }
+
+            return null;
+        }
+
+        public <T> Set<T> asSet(Class<T> clazz)
+        {
+            try {
+                return (Set<T>) this.field.get(obj);
+            } catch (IllegalAccessException e) {
+                AntiCheatAddition.getInstance().getLogger().log(Level.SEVERE, "Unable to get field as set via reflection", e);
+            }
+
+            return null;
+        }
     }
 }

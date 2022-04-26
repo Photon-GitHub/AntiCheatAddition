107f8082348e4153973b697991d62c3f48689dfe
Photon-GitHub <photonde@protonmail.com>
2022-04-26 21:23:31 +0200

    Add NONE permission to eliminate null.

--- src/main/java/de/photon/anticheataddition/InternalPermission.java
+++ src/main/java/de/photon/anticheataddition/InternalPermission.java
@@ -5,6 +5,7 @@ import org.bukkit.permissions.Permissible;
 
 public enum InternalPermission
 {
+    NONE(null),
     BYPASS("anticheataddition.bypass"),
     DEBUG("anticheataddition.debug"),
     INFO("anticheataddition.info"),
@@ -29,19 +30,6 @@ public enum InternalPermission
     public static boolean hasPermission(final Permissible permissible, final String permission)
     {
         return permission == null || permissible.hasPermission(permission);
-    }
-
-    /**
-     * This checks if a {@link Permissible} has a certain permission.
-     *
-     * @param permissible the {@link Permissible} who should be checked
-     * @param permission  the permission that he should be checked for
-     *
-     * @return true if the player has the permission or is op, otherwise false
-     */
-    public static boolean hasPermission(final Permissible permissible, final InternalPermission permission)
-    {
-        return permission == null || permission.hasPermission(permissible);
     }
 
     /**

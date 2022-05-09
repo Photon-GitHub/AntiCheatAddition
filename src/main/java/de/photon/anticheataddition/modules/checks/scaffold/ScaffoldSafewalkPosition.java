f105350b5267f118fb7536ae1ba88474c99d282f
Photon-GitHub <photonde@protonmail.com>
2022-05-09 22:06:03 +0200

    Better formatting.

--- src/main/java/de/photon/anticheataddition/modules/checks/scaffold/ScaffoldSafewalkPosition.java
+++ src/main/java/de/photon/anticheataddition/modules/checks/scaffold/ScaffoldSafewalkPosition.java
@@ -39,8 +39,8 @@ final class ScaffoldSafewalkPosition extends Module
                 // If the player is still sneaking and started long ago they are also bypassed.
                 user.getPlayer().isSneaking() && user.getTimeMap().at(TimeKey.SNEAK_ENABLE).passedTime() > MIN_SNEAK_BYPASS_MILLIS) return 0;
 
-            val xOffset = MathUtil.absDiff(event.getPlayer().getLocation().getX(), event.getBlockAgainst().getX());
-            val zOffset = MathUtil.absDiff(event.getPlayer().getLocation().getZ(), event.getBlockAgainst().getZ());
+            final double xOffset = MathUtil.absDiff(event.getPlayer().getLocation().getX(), event.getBlockAgainst().getX());
+            final double zOffset = MathUtil.absDiff(event.getPlayer().getLocation().getZ(), event.getBlockAgainst().getZ());
             val face = event.getBlock().getFace(event.getBlockAgainst());
 
             // Not building in a straight line.
@@ -58,7 +58,7 @@ final class ScaffoldSafewalkPosition extends Module
 
             if (sneakBorder) {
                 if (user.getDataMap().getCounter(DataKey.Count.SCAFFOLD_SAFEWALK_POSITION_FAILS).incrementCompareThreshold()) {
-                    Log.fine(() -> "Scaffold-Debug | Player: %s has behaviour associated with safe-walk. (Position) | Face: %s | xOffset: %s | zOffset: %s".formatted(user.getPlayer().getName(), face, xOffset, zOffset));
+                    Log.fine(() -> "Scaffold-Debug | Player: %s has behaviour associated with safe-walk. (Position) | Face: %s | xOffset: %f | zOffset: %f".formatted(user.getPlayer().getName(), face, xOffset, zOffset));
                     return 15;
                 }
             } else user.getDataMap().getCounter(DataKey.Count.SCAFFOLD_SAFEWALK_POSITION_FAILS).setToZero();

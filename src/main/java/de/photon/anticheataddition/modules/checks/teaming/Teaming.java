5c2be0ccee5d5bf4fd5324a0ac4e8fd8afb44801
Photon-GitHub <photonde@protonmail.com>
2023-02-10 22:59:59 +0100

    Remove lambda.

--- src/main/java/de/photon/anticheataddition/modules/checks/teaming/Teaming.java
+++ src/main/java/de/photon/anticheataddition/modules/checks/teaming/Teaming.java
@@ -3,9 +3,9 @@ package de.photon.anticheataddition.modules.checks.teaming;
 import com.google.common.base.Preconditions;
 import de.photon.anticheataddition.AntiCheatAddition;
 import de.photon.anticheataddition.modules.ViolationModule;
+import de.photon.anticheataddition.modules.additions.esp.Esp;
 import de.photon.anticheataddition.user.User;
 import de.photon.anticheataddition.user.data.TimeKey;
-import de.photon.anticheataddition.util.datastructure.Pair;
 import de.photon.anticheataddition.util.datastructure.kdtree.QuadTreeSet;
 import de.photon.anticheataddition.util.mathematics.TimeUtil;
 import de.photon.anticheataddition.util.messaging.Log;
@@ -13,7 +13,6 @@ import de.photon.anticheataddition.util.minecraft.world.Region;
 import de.photon.anticheataddition.util.violationlevels.Flag;
 import de.photon.anticheataddition.util.violationlevels.ViolationLevelManagement;
 import de.photon.anticheataddition.util.violationlevels.ViolationManagement;
-import lombok.val;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -34,11 +33,10 @@ public final class Teaming extends ViolationModule implements Listener
 
     private Set<Region> loadSafeZones()
     {
-        Set<Region> safeZones = new HashSet<>();
+        final Set<Region> safeZones = new HashSet<>();
         for (final String s : loadStringList(".safe_zones")) {
             try {
-                Region region = Region.parseRegion(s);
-                safeZones.add(region);
+                safeZones.add(Region.parseRegion(s));
             } catch (NullPointerException e) {
                 Log.severe(() -> "Unable to load safe zone \"" + s + "\" in teaming check, is the world correct?");
             } catch (ArrayIndexOutOfBoundsException e) {
@@ -50,7 +48,7 @@ public final class Teaming extends ViolationModule implements Listener
 
     private Set<World> loadEnabledWorlds()
     {
-        Set<World> worlds = new HashSet<>();
+        final Set<World> worlds = new HashSet<>();
         for (final String key : loadStringList(".enabled_worlds")) {
             final var world = Bukkit.getWorld(key);
             if (world == null) {
@@ -65,8 +63,8 @@ public final class Teaming extends ViolationModule implements Listener
     @Override
     public void enable()
     {
-        val safeZones = loadSafeZones();
-        val enabledWorlds = loadEnabledWorlds();
+        final var safeZones = loadSafeZones();
+        final var enabledWorlds = loadEnabledWorlds();
 
         final double proximityRange = loadDouble(".proximity_range", 4.5);
         final double proximityRangeSquared = proximityRange * proximityRange;
@@ -80,32 +78,32 @@ public final class Teaming extends ViolationModule implements Listener
         Bukkit.getScheduler().scheduleSyncRepeatingTask(
                 AntiCheatAddition.getInstance(),
                 () -> {
-                    val quadTree = new QuadTreeSet<Player>();
+                    final var quadTree = new QuadTreeSet<Player>();
 
                     for (World world : enabledWorlds) {
-                        world.getPlayers().stream()
-                             .map(User::getUser)
-                             .filter(user -> !User.isUserInvalid(user, this))
-                             // Correct game modes.
-                             .filter(User::inAdventureOrSurvivalMode)
-                             // Not engaged in pvp.
-                             .filter(user -> user.getTimeMap().at(TimeKey.COMBAT).notRecentlyUpdated(noPvpTime))
-                             // Get the player's location.
-                             .map(user -> Pair.of(user, user.getPlayer().getLocation()))
-                             // Not in a bypassed region.
-                             .filter(pair -> safeZones.stream().noneMatch(safeZone -> safeZone.isInsideRegion(pair.second())))
-                             // Add the player to the QuadTree.
-                             .forEach(pair -> quadTree.add(pair.second().getX(), pair.second().getZ(), pair.first().getPlayer()));
+                        for (Player player : world.getPlayers()) {
+                            final User user = User.getUser(player);
+                            if (!User.isUserInvalid(user, Esp.INSTANCE)
+                                // Correct game modes.
+                                && user.inAdventureOrSurvivalMode()
+                                // Not engaged in pvp.
+                                && user.getTimeMap().at(TimeKey.COMBAT).notRecentlyUpdated(noPvpTime))
+                            {
+                                final var loc = player.getLocation();
+                                // Not in a bypassed region.
+                                if (safeZones.stream().noneMatch(safeZone -> safeZone.isInsideRegion(loc))) quadTree.add(loc.getX(), loc.getZ(), player);
+                            }
+                        }
 
                         while (!quadTree.isEmpty()) {
                             // Use getAny() so the node itself is contained in the team below.
-                            val firstNode = quadTree.getAny();
-                            val team = quadTree.queryCircle(firstNode, proximityRange).stream()
-                                               // Check for y-distance.
-                                               .filter(node -> node.element().getLocation().distanceSquared(firstNode.element().getLocation()) <= proximityRangeSquared)
-                                               .peek(quadTree::remove)
-                                               .map(QuadTreeSet.Node::element)
-                                               .collect(Collectors.toUnmodifiableSet());
+                            final var firstNode = quadTree.getAny();
+                            final var team = quadTree.queryCircle(firstNode, proximityRange).stream()
+                                                     // Check for y-distance.
+                                                     .filter(node -> node.element().getLocation().distanceSquared(firstNode.element().getLocation()) <= proximityRangeSquared)
+                                                     .peek(quadTree::remove)
+                                                     .map(QuadTreeSet.Node::element)
+                                                     .collect(Collectors.toUnmodifiableSet());
 
                             // Team is too big
                             final int vl = team.size() - allowedSize;

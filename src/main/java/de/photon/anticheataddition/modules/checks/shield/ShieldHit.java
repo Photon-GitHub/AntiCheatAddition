5becaccec389f094fa5b5d8a1e9d97a5373d0ba2
Photon-GitHub <photonde@protonmail.com>
2023-02-24 19:24:06 +0100

    Add ShieldHit check.

--- src/main/java/de/photon/anticheataddition/modules/checks/shield/ShieldHit.java
+++ src/main/java/de/photon/anticheataddition/modules/checks/shield/ShieldHit.java
@@ -1,5 +1,7 @@
-package de.photon.anticheataddition.modules.checks.inventory;
+package de.photon.anticheataddition.modules.checks.shield;
 
+import de.photon.anticheataddition.ServerVersion;
+import de.photon.anticheataddition.modules.ModuleLoader;
 import de.photon.anticheataddition.modules.ViolationModule;
 import de.photon.anticheataddition.user.User;
 import de.photon.anticheataddition.util.violationlevels.Flag;
@@ -11,36 +13,32 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
-public final class InventoryHit extends ViolationModule implements Listener
+public class ShieldHit extends ViolationModule implements Listener
 {
-    public static final InventoryHit INSTANCE = new InventoryHit();
+    public static final ShieldHit INSTANCE = new ShieldHit();
 
     private final int cancelVl = loadInt(".cancel_vl", 60);
 
-    private InventoryHit()
+    private ShieldHit()
     {
-        super("Inventory.parts.Hit");
+        super("Shield.parts.Hit");
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event)
     {
-        // Is a hit-attack
         if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK &&
             event.getDamager() instanceof Player player)
         {
             final var user = User.getUser(player.getUniqueId());
             if (User.isUserInvalid(user, this)) return;
 
-            // Is in Inventory (Detection)
-            if (user.hasOpenInventory() &&
-                // Have the inventory opened for some time
-                user.notRecentlyOpenedInventory(1000))
-            {
+            // Player is blocking and hitting at the same time.
+            if (player.isBlocking()) {
                 this.getManagement().flag(Flag.of(user)
                                               .setAddedVl(20)
                                               .setCancelAction(cancelVl, () -> event.setCancelled(true))
-                                              .setDebug(() -> "Inventory-Debug | Player: " + user.getPlayer().getName() + " hit an entity while having an open inventory."));
+                                              .setDebug(() -> "Shield-Debug | Player: " + user.getPlayer().getName() + " hit an entity while blocking with a shield."));
             }
         }
     }
@@ -51,5 +49,13 @@ public final class InventoryHit extends ViolationModule implements Listener
         return ViolationLevelManagement.builder(this)
                                        .emptyThresholdManagement()
                                        .withDecay(160, 2).build();
+    }
+
+    @Override
+    protected ModuleLoader createModuleLoader()
+    {
+        return ModuleLoader.builder(this)
+                           .setAllowedServerVersions(ServerVersion.MC115.getSupVersionsFrom())
+                           .build();
     }
 }

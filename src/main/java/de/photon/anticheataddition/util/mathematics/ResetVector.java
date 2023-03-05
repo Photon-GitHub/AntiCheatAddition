a0a01ce8624b08f4d5ccdab51cbec740968d339b
Photon-GitHub <photonde@protonmail.com>
2023-03-05 21:36:26 +0100

    Code cleanup.

--- src/main/java/de/photon/anticheataddition/util/mathematics/ResetVector.java
+++ src/main/java/de/photon/anticheataddition/util/mathematics/ResetVector.java
@@ -31,16 +31,4 @@ public final class ResetVector extends Vector
         this.z = this.resetZ;
         return this;
     }
-
-    @Override
-    public boolean equals(Object o)
-    {
-        return super.equals(o);
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return super.hashCode();
-    }
 }

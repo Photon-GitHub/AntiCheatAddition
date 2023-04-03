14d6f6bef15b987dce4ac9a1094c854c66fde87f
Photon-GitHub <photonde@protonmail.com>
2023-04-03 21:28:28 +0200

    Hide empty threshold management.

--- src/main/java/de/photon/anticheataddition/util/violationlevels/threshold/EmptyThresholds.java
+++ src/main/java/de/photon/anticheataddition/util/violationlevels/threshold/EmptyThresholds.java
@@ -1,10 +1,15 @@
 package de.photon.anticheataddition.util.violationlevels.threshold;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 final class EmptyThresholds implements ThresholdManagement
 {
+    static final EmptyThresholds INSTANCE = new EmptyThresholds();
+
     @Override
     public void executeThresholds(int fromVl, int toVl, @NotNull Player players)
     {

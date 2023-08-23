b009fbf4e3bc2b7ae0cc2ee3df80640f81520c78
Photon-GitHub <photonde@protonmail.com>
2023-08-23 02:00:39 +0200

    Add placement against air / liquid check.
    Signed-off-by: Photon-GitHub <photonde@protonmail.com>

--- src/main/java/de/photon/anticheataddition/modules/checks/scaffold/ScaffoldPosition.java
+++ src/main/java/de/photon/anticheataddition/modules/checks/scaffold/ScaffoldPosition.java
@@ -3,6 +3,7 @@ package de.photon.anticheataddition.modules.checks.scaffold;
 import de.photon.anticheataddition.modules.Module;
 import de.photon.anticheataddition.util.mathematics.MathUtil;
 import de.photon.anticheataddition.util.messaging.Log;
+import de.photon.anticheataddition.util.minecraft.world.MaterialUtil;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 /**
@@ -21,12 +22,17 @@ final class ScaffoldPosition extends Module
     {
         if (!this.isEnabled()) return 0;
 
+        if (MaterialUtil.isAir(event.getBlockAgainst().getType()) || MaterialUtil.LIQUIDS.contains(event.getBlockAgainst().getType())) {
+            Log.fine(() -> "Scaffold-Debug | Player: " + event.getPlayer().getName() + " placed against air or liquid.");
+            return 30;
+        }
+
         // This sorts out scaffolding with non-full block hitboxes that will cause false positives (e.g. fences).
         if (!event.getBlockPlaced().getType().isOccluding()) return 0;
 
         final var face = event.getBlock().getFace(event.getBlockAgainst());
         if (face == null) {
-            Log.fine(() -> "Scaffold-Debug | Player: " + event.getPlayer().getName() + " placed against missing block.");
+            Log.fine(() -> "Scaffold-Debug | Player: " + event.getPlayer().getName() + " placed against distant block.");
             return 30;
         }
 

42a9ebac43cf1b6aaedb5fbd0913c5d708666644
Photon-GitHub <photonde@protonmail.com>
2024-05-14 23:20:56 +0200

    Add comment.
    Signed-off-by: Photon-GitHub <photonde@protonmail.com>

--- src/main/java/de/photon/anticheataddition/modules/checks/inventory/StatisticalBatchProcessor.java
+++ src/main/java/de/photon/anticheataddition/modules/checks/inventory/StatisticalBatchProcessor.java
@@ -76,7 +76,7 @@ public final class StatisticalBatchProcessor extends AsyncBatchProcessor<Invento
 
         Log.finest(() -> "Inventory-Debug | Statistical Player: %s | RAW-OFFSET: %s | SCALED-OFFSET: %s".formatted(user.getPlayer().getName(), Arrays.toString(timeOffsets), Arrays.toString(normalizedOffsets)));
 
-        // Perform the K-S test
+        // Perform the K-S test to see if the clicks are uniformly distributed.
         final double d_max = KolmogorovSmirnow.kSTestForUniformDistribution(normalizedOffsets);
 
         Log.finer(() -> "Inventory-Debug | Statistical Player: %s, D_MAX: %f, D_TEST: %f".formatted(user.getPlayer().getName(), d_max, D_TEST));
